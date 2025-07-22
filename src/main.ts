565a510910884d29b68e836f1029d7d772413b37
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-07-22 09:56:52 -0500

    fix: handle application shutdown on process signals and errors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let app: INestApplication<any>;
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+process.on('SIGINT', async () => {
+    await app.close();
+    console.log('Closing application (SIGINT)');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+process.on('SIGTERM', async () => {
+    await app.close();
+    console.log('Closing application (SIGTERM)');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+process.on('unhandledException', async () => {
+    await app.close();
+    console.log('Closing application due unhandledException');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+process.on('unhandledRejection', async () => {
+    await app.close();
+    console.log('Closing application due unhandledRejection');
+});
+
 async function bootstrap() {
     process.env.NEST_DEBUG = 'true';
-    const app = await NestFactory.create(AppModule);
+    app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT ?? 3000);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
