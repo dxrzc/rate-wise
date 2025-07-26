1325569ba1305993065c307a33e88c254ae6f1d0
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-07-26 12:53:04 -0500

    feat: update AppModule to include entities and correct schema file path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ItemsModule } from './items/items.module';
+import { Item } from './items/entities/item.entity';
+import { User } from './users/entities/user.entity';
 import { AppConfigModule } from './config/app-config.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { DatabaseConfigService } from './config/services/postgres-db.config.service';
@@ -15,19 +18,24 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
             inject: [DatabaseConfigService],
             useFactory: (dbConfigService: DatabaseConfigService) => ({
                 type: 'postgres',
+                entities: [User, Item],
                 url: dbConfigService.uri,
-                entities: [],
                 autoLoadEntities: false,
                 retryAttempts: 3,
                 retryDelay: 1000,
+                synchronize: true,
             }),
         }),
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             playground: false,
-            autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+            autoSchemaFile: join(
+                process.cwd(),
+                'src/common/graphql/schema.gql',
+            ),
             plugins: [ApolloServerPluginLandingPageLocalDefault()],
         }),
+        ItemsModule,
     ],
 })
 export class AppModule {}
