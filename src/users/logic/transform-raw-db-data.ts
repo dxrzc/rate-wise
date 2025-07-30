c0998b5cffab3b3efd68544ef6c702107e0f739d
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-07-30 11:33:50 -0500

    chore: rename transfromRawDbData function to transformUserRawDbData for consistency

--- src/users/logic/transform-raw-db-data.ts
+++ src/users/logic/transform-raw-db-data.ts
@@ -1,7 +1,7 @@
 import { User } from '../entities/user.entity';
 import { IUserDbRecord } from '../interfaces/user-db-record.interface';
 
-export function transformRawDbData(rawData: IUserDbRecord): User {
+export function transformUserRawDbData(rawData: IUserDbRecord): User {
     return {
         createdAt: rawData.created_at,
         reputationScore: rawData.reputation_score,
