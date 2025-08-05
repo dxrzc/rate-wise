d6df528f1e66f5d0647ba59727d50fdf1d3dc30d
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-05 17:33:44 -0500

    feat: add redis uri to joi validation schema

--- src/config/schemas/config-validation.schema.ts
+++ src/config/schemas/config-validation.schema.ts
@@ -3,6 +3,7 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
     PORT: Joi.number().port().default(3000),
     POSTGRES_URI: Joi.string().uri().required(),
+    REDIS_URI: Joi.string().uri().required(),
     NODE_ENV: Joi.string()
         .valid('development', 'integration', 'e2e')
         .required(),
