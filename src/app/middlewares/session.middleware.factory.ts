7fa3e515b2ab847e12ae5a7a3d568ddc3b8c5fc7
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-05 18:46:45 -0500

    fix: use an enum for environment env

--- src/app/middlewares/session.middleware.factory.ts
+++ src/app/middlewares/session.middleware.factory.ts
@@ -2,6 +2,7 @@ import * as session from 'express-session';
 import { createClient } from 'redis';
 import { RedisStore } from 'connect-redis';
 import { Injectable } from '@nestjs/common';
+import { Environment } from 'src/common/enum/environment.enum';
 import { RedisConfigService } from 'src/config/services/redis-config.service';
 import { SessionConfigService } from 'src/config/services/session-config.service';
 import { ServerConfigService } from 'src/config/services/server-config.service';
@@ -9,14 +10,14 @@ import { ServerConfigService } from 'src/config/services/server-config.service';
 @Injectable()
 export class SessionMiddlewareFactory {
     constructor(
-        private readonly redisConfigService: RedisConfigService,
-        private readonly sessionConfigService: SessionConfigService,
-        private readonly serverConfigService: ServerConfigService,
+        private readonly redisConfig: RedisConfigService,
+        private readonly sessionConfig: SessionConfigService,
+        private readonly serverConfig: ServerConfigService,
     ) {}
 
     async create() {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-        const redisClient = createClient({ url: this.redisConfigService.uri });
+        const redisClient = createClient({ url: this.redisConfig.uri });
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
         await redisClient.connect();
 
@@ -31,12 +32,13 @@ export class SessionMiddlewareFactory {
             store: redisStore,
             resave: false,
             saveUninitialized: false,
-            secret: this.sessionConfigService.cookieSecret,
+            secret: this.sessionConfig.cookieSecret,
             unset: 'destroy',
             cookie: {
                 httpOnly: true,
-                secure: this.serverConfigService.environment === 'production',
-                maxAge: this.sessionConfigService.cookieMaxAge,
+                secure:
+                    this.serverConfig.environment === Environment.PRODUCTION,
+                maxAge: this.sessionConfig.cookieMaxAge,
             },
             rolling: true,
         });
