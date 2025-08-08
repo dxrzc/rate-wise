e5a4a7e8dc7ad80a109e36865e655f25b7796109
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-08 12:51:00 -0500

    feat: config redis suscriber as a provider

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { RedisSuscriber } from './suscribers/redis.suscriber';
 import { ConfigurableModuleClass } from './redis.module-definition';
 
 @Module({
-    providers: [RedisService],
+    providers: [RedisService, RedisSuscriber],
     exports: [RedisService],
 })
 export class RedisModule extends ConfigurableModuleClass {}
