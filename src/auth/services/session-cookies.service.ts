5eb25fed66ff5f30f03e35b228f85f2d2f897f4a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-08 12:52:06 -0500

    feat: create the user-session-relation when a new session is created

--- src/auth/services/session-cookies.service.ts
+++ src/auth/services/session-cookies.service.ts
@@ -4,14 +4,25 @@ import { RedisService } from 'src/redis/redis.service';
 import { makeSessionsIndexKey } from '../functions/make-sessions-index-key';
 import { ISessionData } from 'src/common/interfaces/cookies/session-data.interface';
 import { regenerateCookie } from '../functions/regenerate-cookie';
+import { makeUserSessionRelationKey } from '../functions/make-user-session-relation-key';
 
 @Injectable()
 export class SessionCookiesService {
     constructor(private readonly redisService: RedisService) {}
 
+    private async createSessionUserRelation(userId: string, sessionId: string) {
+        await this.redisService.set(
+            makeUserSessionRelationKey(sessionId),
+            userId,
+        );
+    }
+
     async newSession(req: Request & { session: ISessionData }, userId: string) {
         const sessionsIndexKey = makeSessionsIndexKey(userId);
         const sessionId = await regenerateCookie(req, userId);
-        await this.redisService.addToSet(sessionsIndexKey, sessionId);
+        await Promise.all([
+            this.createSessionUserRelation(userId, sessionId),
+            this.redisService.addToSet(sessionsIndexKey, sessionId),
+        ]);
     }
 }
