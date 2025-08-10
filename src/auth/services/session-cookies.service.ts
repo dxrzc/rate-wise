06120308aa7f7a3055e8a6cf86325b85666270b5
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-10 16:06:30 -0500

    feat: refactor and add new methods to delete the session in sessionService

--- src/auth/services/session-cookies.service.ts
+++ src/auth/services/session-cookies.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Request } from 'express';
 import { RedisService } from 'src/redis/redis.service';
 import { RequestContext } from '../types/request-context.type';
-import { regenerateCookie } from '../functions/regenerate-cookie';
+import { promisify } from 'src/common/functions/utils/promisify.util';
 import { makeSessionsIndexKey } from '../functions/make-sessions-index-key';
 import { makeUserSessionRelationKey } from '../functions/make-user-session-relation-key';
 
@@ -10,6 +9,7 @@ import { makeUserSessionRelationKey } from '../functions/make-user-session-relat
 export class SessionsService {
     constructor(private readonly redisService: RedisService) {}
 
+    // creates a session-user relation to track down sessions and their respective owners
     private async createSessionUserRelation(userId: string, sessionId: string) {
         await this.redisService.set(
             makeUserSessionRelationKey(sessionId),
@@ -17,16 +17,33 @@ export class SessionsService {
         );
     }
 
+    // adds the sessionID to a set containing all the sessions belonging to a user
+    private async addSessionToUserSet(userId: string, sessionID: string) {
+        await this.redisService.addToSet(
+            makeSessionsIndexKey(userId),
+            sessionID,
+        );
+    }
+
+    // generate a new session id preventing session fixation
+    private async regenerateSession(req: RequestContext): Promise<void> {
+        await promisify<void>((cb) => req.session.regenerate(cb));
+    }
+
     async activeSessions(userId: string): Promise<number> {
         return await this.redisService.getSetSize(makeSessionsIndexKey(userId));
     }
 
+    async deleteSession(req: RequestContext) {
+        await promisify<void>((cb) => req.session.destroy(cb));
+    }
+
     async newSession(req: RequestContext, userId: string) {
-        const sessionsIndexKey = makeSessionsIndexKey(userId);
-        const sessionId = await regenerateCookie(req, userId);
+        await this.regenerateSession(req);
+        req.session.userId = userId;
         await Promise.all([
-            this.createSessionUserRelation(userId, sessionId),
-            this.redisService.addToSet(sessionsIndexKey, sessionId),
+            this.createSessionUserRelation(userId, req.sessionID),
+            this.addSessionToUserSet(userId, req.sessionID),
         ]);
     }
 }
