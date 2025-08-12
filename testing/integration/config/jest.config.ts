191d18618c2df8b264f3ab77a36090f726a01a27
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-11 21:49:35 -0500

    feat: add integration jest config

---
+++ testing/integration/config/jest.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    rootDir: process.cwd(), // workidr
+
+    // setup
+    setupFilesAfterEnv: [
+        '<rootDir>/testing/integration/config/setupAfterEnv.ts',
+    ],
+
+    // coverage
+    collectCoverage: true,
+    // collectCoverageFrom: [
+    //     '<rootDir>/src/**/*.ts',
+    //     '!<rootDir>/test/integration/**',
+    // ],
+    coverageDirectory: '<rootDir>/coverage/integration',
+    coverageProvider: 'v8',
+
+    // test files
+    roots: ['<rootDir>/testing/integration/specs'],
+
+    // aliases
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+        '^@integration/(.*)$': '<rootDir>/testing/integration/$1',
+        '^@queries/(.*)$': '<rootDir>/testing/queries/$1',
+    },
+
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-node',
+
+    maxWorkers: '50%',
+};
+
+export default config;
