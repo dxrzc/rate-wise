30a9de1c23258ea2c16eab8e5f53afb07f0101ec
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-12 18:43:26 -0500

    fix: add userSeed in testKit interface

--- testing/integration/interfaces/test-kit.interface.ts
+++ testing/integration/interfaces/test-kit.interface.ts
@@ -1,6 +1,8 @@
 import { INestApplication } from '@nestjs/common';
+import { UserSeedService } from 'src/seed/services/user-seed.service';
 import { App } from 'supertest/types';
 
 export interface ITestKit {
     app: INestApplication<App>;
+    userSeed: UserSeedService;
 }
