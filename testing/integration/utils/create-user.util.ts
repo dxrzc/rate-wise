41c1e919f05289902c37506ade314eaab5332334
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-13 16:07:26 -0500

    feat: add create-user util for integration tests

---
+++ testing/integration/utils/create-user.util.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import * as request from 'supertest';
+import { testKit } from './test-kit.util';
+import { createQuery } from './create-query.util';
+import { signUpQuery } from '@queries/sign-up.query';
+import { UserModel } from 'src/users/models/user.model';
+
+export async function createUser(): Promise<UserModel & { password: string }> {
+    const user = testKit.userSeed.signUpInput;
+    const res = await request(testKit.app.getHttpServer())
+        .post('/graphql')
+        .send(createQuery(signUpQuery, user));
+    return {
+        ...(res.body.data.signUp as UserModel),
+        password: user.password,
+    };
+}
