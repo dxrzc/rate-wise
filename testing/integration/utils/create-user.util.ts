4e0a1cc13dd3ef03e7eb65d26b39cef23390b79e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-14 12:59:13 -0500

    testing(int): return session cookie in create-user util

--- testing/integration/utils/create-user.util.ts
+++ testing/integration/utils/create-user.util.ts
@@ -4,8 +4,14 @@ import { testKit } from './test-kit.util';
 import { createQuery } from './create-query.util';
 import { signUpQuery } from '@queries/sign-up.query';
 import { UserModel } from 'src/users/models/user.model';
+import { getSessionCookie } from './get-session-cookie.util';
 
-export async function createUser(): Promise<UserModel & { password: string }> {
+interface ExtraData {
+    password: string;
+    sessionCookie: string;
+}
+
+export async function createUser(): Promise<UserModel & ExtraData> {
     const user = testKit.userSeed.signUpInput;
     const res = await request(testKit.app.getHttpServer())
         .post('/graphql')
@@ -13,5 +19,6 @@ export async function createUser(): Promise<UserModel & { password: string }> {
     return {
         ...(res.body.data.signUp as UserModel),
         password: user.password,
+        sessionCookie: getSessionCookie(res),
     };
 }
