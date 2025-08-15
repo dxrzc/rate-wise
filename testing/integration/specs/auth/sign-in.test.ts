f0c2833353f94bb331eebfeaa303a6f0c673f1ef
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-15 12:54:19 -0500

    fix(int): add missing tests in sign-in.test

--- testing/integration/specs/auth/sign-in.test.ts
+++ testing/integration/specs/auth/sign-in.test.ts
@@ -1,15 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { makeUserSessionRelationKey } from 'src/auth/functions/make-user-session-relation-key';
+import { makeSessionsIndexKey } from 'src/auth/functions/make-sessions-index-key';
+import { getSidFromCookie } from '@integration/utils/get-sid-from-cookie.util';
+import { getSessionCookie } from '@integration/utils/get-session-cookie.util';
+import { PASSWORD_MAX_LENGTH } from 'src/auth/constants/auth.constants';
 import { createQuery } from '@integration/utils/create-query.util';
 import { createUser } from '@integration/utils/create-user.util';
 import { testKit } from '@integration/utils/test-kit.util';
 import { signInQuery } from '@queries/sign-in.query';
 import { Code } from '@integration/enum/code.enum';
+import { faker } from '@faker-js/faker/.';
 import * as request from 'supertest';
 import {
     INVALID_CREDENTIALS,
     MAX_SESSIONS_REACHED,
 } from 'src/auth/constants/errors.constants';
-import { faker } from '@faker-js/faker/.';
-import { PASSWORD_MAX_LENGTH } from 'src/auth/constants/auth.constants';
 
 describe('signIn', () => {
     describe('Successful sign-in', () => {
@@ -25,6 +30,44 @@ describe('signIn', () => {
                 );
             expect(res).notToFail();
             expect(res).toContainCookie(testKit.sessConfig.cookieName);
+        });
+
+        test('should add the new session to the user sessions index redis set', async () => {
+            const { email, password } = await createUser();
+            const res = await request(testKit.app.getHttpServer())
+                .post('/graphql')
+                .send(
+                    createQuery(signInQuery, {
+                        password,
+                        email,
+                    }),
+                );
+            expect(res).notToFail();
+            const key = makeSessionsIndexKey(res.body.data.signIn.id as string);
+            const sessSet = await testKit.redisService.setMembers(key);
+            expect(sessSet.length).toBe(2); // signUp and signIn
+            expect(
+                sessSet.find(
+                    (key) => key === getSidFromCookie(getSessionCookie(res)),
+                ),
+            ).toBeDefined();
+        });
+
+        test('should create session-user relation record in redis', async () => {
+            const { email, password } = await createUser();
+            const res = await request(testKit.app.getHttpServer())
+                .post('/graphql')
+                .send(
+                    createQuery(signInQuery, {
+                        password,
+                        email,
+                    }),
+                );
+            expect(res).notToFail();
+            const sid = getSidFromCookie(getSessionCookie(res));
+            const key = makeUserSessionRelationKey(sid);
+            const sessionOwner = await testKit.redisService.get(key);
+            expect(sessionOwner).toBe(res.body.data.signIn.id);
         });
     });
 
