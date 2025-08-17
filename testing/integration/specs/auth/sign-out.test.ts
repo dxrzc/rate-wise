4a1bc15dbce8d874af40a5874771320c3f8bf6f3
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-17 12:59:14 -0500

    feat(int): enhance sign-out.test coverage

--- testing/integration/specs/auth/sign-out.test.ts
+++ testing/integration/specs/auth/sign-out.test.ts
@@ -1,5 +1,7 @@
 import { AUTHENTICATION_REQUIRED } from 'src/auth/constants/errors.constants';
+import { getSidFromCookie } from '@integration/utils/get-sid-from-cookie.util';
 import { createQuery } from '@integration/utils/create-query.util';
+import { createUser } from '@integration/utils/create-user.util';
 import { testKit } from '@integration/utils/test-kit.util';
 import { signOutQuery } from '@queries/sign-out.query';
 import { Code } from '@integration/enum/code.enum';
@@ -15,6 +17,25 @@ describe('signOut', () => {
                 Code.UNAUTHENTICATED,
                 AUTHENTICATION_REQUIRED,
             );
+        });
+    });
+
+    describe('Successful signOut', () => {
+        test('session cookie should be removed from redis store', async () => {
+            // sign up
+            const { sessionCookie } = await createUser();
+            // sign out
+            const res = await request(testKit.app.getHttpServer())
+                .post('/graphql')
+                .set('Cookie', sessionCookie)
+                .send(createQuery(signOutQuery, {}));
+            expect(res).notToFail();
+            // cookie should be removed from redis
+            await expect(
+                testKit.redisService.get(
+                    `session:${getSidFromCookie(sessionCookie)}`,
+                ),
+            ).resolves.toBeNull();
         });
     });
 });
