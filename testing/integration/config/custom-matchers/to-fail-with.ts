0c7e2d6124cc0fa5b37b0bfefa874c04ec9e0e27
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-17 12:31:37 -0500

    feat(int): use "equals" instead of === to allow expect.any(..) in "toFailWith" custom jest matcher

--- testing/integration/config/custom-matchers/to-fail-with.ts
+++ testing/integration/config/custom-matchers/to-fail-with.ts
@@ -1,8 +1,8 @@
 import type { MatcherFunction } from 'expect';
 import { GraphQLErrorResponse } from '@integration/interfaces/error-response.interface';
 
-export const toFailWith: MatcherFunction<[code: string, message: string]> =
-    function (response: unknown, code: string, message: string) {
+export const toFailWith: MatcherFunction<[code: unknown, message: unknown]> =
+    function (response: unknown, code: unknown, message: unknown) {
         const typedResponse = response as { body: GraphQLErrorResponse };
 
         if (!typedResponse.body)
@@ -13,9 +13,6 @@ export const toFailWith: MatcherFunction<[code: string, message: string]> =
         if (typeof body !== 'object' || !body.errors)
             throw new TypeError('body must be a valid response body');
 
-        if (typeof code !== 'string' || typeof message !== 'string')
-            throw new TypeError('code and message must be of type string');
-
         if (body.errors.length > 1)
             throw new Error('More than one error was found in the body');
 
@@ -23,10 +20,14 @@ export const toFailWith: MatcherFunction<[code: string, message: string]> =
             throw new Error('No error was found in the body');
 
         const error = body.errors.at(0)!;
-        const pass = error.code === code && error.message === message;
+
+        const pass =
+            this.equals(error.code, code) &&
+            this.equals(error.message, message);
 
         if (pass) {
             return {
+                pass: true,
                 message: () =>
                     `Expected response **not** to match error:\n` +
                     `  code: ${this.utils.printExpected(code)}\n` +
@@ -34,10 +35,10 @@ export const toFailWith: MatcherFunction<[code: string, message: string]> =
                     `But received:\n` +
                     `  code: ${this.utils.printReceived(error.code)}\n` +
                     `  message: ${this.utils.printReceived(error.message)}`,
-                pass: true,
             };
         } else {
             return {
+                pass: false,
                 message: () =>
                     `Expected response to match error:\n` +
                     `  code: ${this.utils.printExpected(code)}\n` +
@@ -45,7 +46,6 @@ export const toFailWith: MatcherFunction<[code: string, message: string]> =
                     `But received:\n` +
                     `  code: ${this.utils.printReceived(error.code)}\n` +
                     `  message: ${this.utils.printReceived(error.message)}`,
-                pass: false,
             };
         }
     };
