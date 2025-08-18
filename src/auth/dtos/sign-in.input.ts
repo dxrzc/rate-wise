3bd9e95aed66f1cfd703d2f1e921507f82f9fd41
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-18 15:43:51 -0500

    refactor(inputs): use mapped types

--- src/auth/dtos/sign-in.input.ts
+++ src/auth/dtos/sign-in.input.ts
@@ -1,11 +1,15 @@
-import { Field, InputType } from '@nestjs/graphql';
 import { IsEmail, IsString, MaxLength } from 'class-validator';
-import { PASSWORD_MAX_LENGTH } from '../constants/auth.constants';
+import { Field, InputType } from '@nestjs/graphql';
+import {
+    MAX_EMAIL_LENGTH,
+    PASSWORD_MAX_LENGTH,
+} from '../constants/auth.constants';
 
 @InputType()
 export class SignInInput {
     @IsString()
     @IsEmail()
+    @MaxLength(MAX_EMAIL_LENGTH)
     @Field(() => String)
     email!: string;
 
