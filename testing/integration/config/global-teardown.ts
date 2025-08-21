f252c8a7ca25dafb184cc38b58eb8bc613ee7127
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-21 10:26:53 -0500

    feat(int): run db migrations before tests

--- testing/integration/config/global-teardown.ts
+++ testing/integration/config/global-teardown.ts
@@ -3,7 +3,6 @@ import * as path from 'path';
 
 export default async function () {
     await compose.downMany(global.services as string[], {
-        log: true,
         cwd: path.join(__dirname),
     });
 }
