5f3187615e8fe4cceab03374f92f36e93b012d3a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-21 08:38:49 -0500

    fix(migration script): eslint error

--- scripts/generate-migration.js
+++ scripts/generate-migration.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process');
-
 const args = process.argv.slice(2); // migration name
 if (args.length === 0) {
     console.error('Migration name not provided');
@@ -10,5 +9,4 @@ const migrationName = args[0];
 const migrationPath = `db/migrations/${migrationName}`;
 
 const command = `npx typeorm -d dist/db/data-source.dev.js migration:generate ${migrationPath}`;
-console.log(`📦 Running: ${command}`);
 execSync(command, { stdio: 'inherit' });
