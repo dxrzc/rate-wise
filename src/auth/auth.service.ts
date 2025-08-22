29a8625d31df19bcb6acc8a8d946807e1e03cfee
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-22 18:42:15 -0500

    feat(logging): add logs in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,24 @@
-import { SessionConfigService } from 'src/config/services/session-config.service';
-import { ServerConfigService } from 'src/config/services/server-config.service';
-import { ReAuthenticationInput } from './dtos/re-authentication.input';
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { RequestContext } from './types/request-context.type';
-import { SessionService } from './services/session.service';
-import { UsersService } from 'src/users/users.service';
-import { User } from 'src/users/entities/user.entity';
-import { SignInInput } from './dtos/sign-in.input';
-import { SignUpInput } from './dtos/sign-up.input';
-import * as bcrypt from 'bcryptjs';
 import {
     INVALID_CREDENTIALS,
     MAX_SESSIONS_REACHED,
 } from './constants/errors.constants';
+import {
+    BadRequestException,
+    Inject,
+    Injectable,
+    Logger,
+} from '@nestjs/common';
+import { SessionConfigService } from 'src/config/services/session-config.service';
+import { ServerConfigService } from 'src/config/services/server-config.service';
+import { ReAuthenticationInput } from './dtos/re-authentication.input';
+import { RequestContext } from './types/request-context.type';
+import { SessionService } from './services/session.service';
+import { UsersService } from 'src/users/users.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { User } from 'src/users/entities/user.entity';
+import { SignInInput } from './dtos/sign-in.input';
+import { SignUpInput } from './dtos/sign-up.input';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -21,6 +27,8 @@ export class AuthService {
         private readonly serverConfig: ServerConfigService,
         private readonly sessionService: SessionService,
         private readonly userService: UsersService,
+        @Inject(WINSTON_MODULE_PROVIDER)
+        private readonly logger: Logger,
     ) {}
 
     private hashPassword(password: string): string {
@@ -33,41 +41,60 @@ export class AuthService {
     }
 
     async signUp(signUpInput: SignUpInput, req: RequestContext): Promise<User> {
+        this.logger.log(`New account creation attemp for ${signUpInput.email}`);
         signUpInput.password = this.hashPassword(signUpInput.password);
         const user = await this.userService.createOne(signUpInput);
         await this.sessionService.newSession(req, user.id);
+        this.logger.log(`Account ${signUpInput.email} created successfully `);
         return user;
     }
 
     async signIn(credentials: SignInInput, req: RequestContext): Promise<User> {
+        this.logger.log(`Sign in attemp for email ${credentials.email}`);
         const user = await this.userService.findOneByEmail(credentials.email);
-        if (!user || !this.passwordMatches(user.password, credentials.password))
+
+        if (!user) {
+            this.logger.error(`Email not found`);
             throw new BadRequestException(INVALID_CREDENTIALS);
+        }
+
+        if (!this.passwordMatches(user.password, credentials.password)) {
+            this.logger.error('Password does not match');
+            throw new BadRequestException(INVALID_CREDENTIALS);
+        }
 
         const sessions = await this.sessionService.activeSessions(user.id);
         if (sessions === this.sessionConfig.maxUserSessions) {
+            this.logger.error(`Maximum sessions reached for user ${user.id}`);
             throw new BadRequestException(MAX_SESSIONS_REACHED);
         }
 
         await this.sessionService.newSession(req, user.id);
+        this.logger.log(`User ${user.id} signed in successfully`);
         return user;
     }
 
     async signOut(req: RequestContext): Promise<void> {
+        this.logger.log(`Sign out for session ${req.sessionID}`);
         await this.sessionService.deleteSession(req);
+        this.logger.log(`User ${req.session.userId} signed out`);
     }
 
     async signOutAll(
         auth: ReAuthenticationInput,
         userId: string,
     ): Promise<void> {
+        this.logger.log(`Attempting to close all sessions for user: ${userId}`);
         const user = await this.userService.findOneByIdOrThrow(userId);
         const passwordMatches = this.passwordMatches(
             user.password,
             auth.password,
         );
-        if (!passwordMatches)
+        if (!passwordMatches) {
+            this.logger.warn(`Invalid credentials for userId: ${userId}`);
             throw new BadRequestException(INVALID_CREDENTIALS);
+        }
         await this.sessionService.deleteAllSessions(userId);
+        this.logger.log(`All sessions closed for userId: ${userId}`);
     }
 }
