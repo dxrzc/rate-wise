b2a85bc3631c146d4790bd83f35a0489e3ce8412
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-22 20:12:35 -0500

    fix: remove request tracing interceptor

--- src/app/providers/interceptors/app-reqtracing.interceptor.provider.ts
+++
@@ -1,7 +0,0 @@
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { RequestTracingInterceptor } from 'src/common/interceptors/request.interceptor';
-
-export const appRequestTracingInterceptor = {
-    provide: APP_INTERCEPTOR,
-    useClass: RequestTracingInterceptor,
-};
