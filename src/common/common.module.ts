50307d7834a35a8f3ad8a2a1d515c81398a3820e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-25 18:24:45 -0500

    feat(services): add email-service

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HashingService } from './services/hashing.service';
+import { EmailService } from './services/email.service';
 
 @Module({
-    providers: [HashingService],
-    exports: [HashingService],
+    providers: [HashingService, EmailService],
+    exports: [HashingService, EmailService],
 })
 export class CommonModule {}
