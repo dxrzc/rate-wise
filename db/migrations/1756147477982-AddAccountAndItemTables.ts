fcc0d279df741fa8805135b432767cde43ca69f3
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-25 13:46:18 -0500

    fix(migration): regenerate fix migration using "public" to avoid problems in future migrations

--- db/migrations/1756147477982-AddAccountAndItemTables.ts
+++ db/migrations/1756147477982-AddAccountAndItemTables.ts
@@ -1,17 +1,16 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateAccountAndItemTables1755785256321
+export class AddAccountAndItemTables1756147477982
     implements MigrationInterface
 {
-    name = 'CreateAccountAndItemTables1755785256321';
+    name = 'AddAccountAndItemTables1756147477982';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TYPE "account_role_enum" AS ENUM('user','moderator','admin')`,
-        );
-
-        await queryRunner.query(
             `CREATE TABLE "item" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "title" character varying(40) NOT NULL, "description" text NOT NULL, "category" character varying(40) NOT NULL, "tags" character varying(20) array NOT NULL, "average_rating" numeric(3,2) NOT NULL DEFAULT '0', "review_count" integer NOT NULL DEFAULT '0', "user_id" uuid, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`,
+        );
+        await queryRunner.query(
+            `CREATE TYPE "public"."account_role_enum" AS ENUM('user', 'moderator', 'admin')`,
         );
         await queryRunner.query(
             `CREATE TABLE "account" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "username" character varying(30) NOT NULL, "email" character varying(45) NOT NULL, "password" character varying(60) NOT NULL, "role" "public"."account_role_enum" NOT NULL DEFAULT 'user', "reputation_score" integer NOT NULL DEFAULT '0', CONSTRAINT "UQ_41dfcb70af895ddf9a53094515b" UNIQUE ("username"), CONSTRAINT "UQ_4c8f96ccf523e9a3faefd5bdd4c" UNIQUE ("email"), CONSTRAINT "PK_54115ee388cdb6d86bb4bf5b2ea" PRIMARY KEY ("id"))`,
@@ -26,7 +25,7 @@ export class CreateAccountAndItemTables1755785256321
             `ALTER TABLE "item" DROP CONSTRAINT "FK_2f3f2831c9b37214309d23b07fd"`,
         );
         await queryRunner.query(`DROP TABLE "account"`);
+        await queryRunner.query(`DROP TYPE "public"."account_role_enum"`);
         await queryRunner.query(`DROP TABLE "item"`);
-        await queryRunner.query(`DROP TYPE "account_role_enum"`);
     }
 }
