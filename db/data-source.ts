4afdd92ef5d7d7614dd5ad8f71da6185a9884e71
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-08-30 09:45:31 -0500

    chore(docker): run migration when dev container starts

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,8 +1,4 @@
-import * as dotenvExpand from 'dotenv-expand';
 import { DataSource } from 'typeorm';
-import * as dotenv from 'dotenv';
-
-dotenvExpand.expand(dotenv.config({ path: '.env.dev' }));
 
 export default new DataSource({
     url: process.env.POSTGRES_URI,
