3beb3482086d37b9cc63ac768c3e4d55fdf15a59
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-02 19:30:30 -0500

    chore(integration): add logs in db migration

--- testing/integration/config/helpers/run-migration.helper.ts
+++ testing/integration/config/helpers/run-migration.helper.ts
@@ -4,6 +4,8 @@ import { promisify } from 'util';
 const exec = promisify(execCb);
 
 export async function runMigration(targetUri: string) {
+    console.log('\n');
+    console.log('\x1b[34m%s\x1b[0m', 'Running migration...');
     await exec(
         `npx nest build && npx typeorm -d dist/db/data-source.js migration:run`,
         {
@@ -11,4 +13,5 @@ export async function runMigration(targetUri: string) {
             cwd: process.cwd(),
         },
     );
+    console.log('\x1b[32m%s\x1b[0m', 'Migration executed successfully');
 }
