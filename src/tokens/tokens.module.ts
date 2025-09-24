97e071670b2af35bbee6c5ee7ab7bcc0746f3274
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-24 13:32:58 -0500

    chore(tokens): add tokens.module

---
+++ src/tokens/tokens.module.ts
@@ -0,0 +1,47 @@
+import { ISessionsModuleOptions } from 'src/sessions/interface/sessions-module-options.interface';
+import { REDIS_FOR_TOKENS } from './constants/redis-tokens-client.token.constant';
+import { RedisAdapter } from 'src/common/redis/redis.adapter';
+import { TokensService } from './tokens.service';
+import {
+    ConfigurableModuleAsyncOptions,
+    DynamicModule,
+    Global,
+    Module,
+} from '@nestjs/common';
+
+@Global()
+@Module({})
+export class TokensModule {
+    static forRootAsync(
+        options: ConfigurableModuleAsyncOptions<ISessionsModuleOptions>,
+    ): DynamicModule {
+        if (options.useFactory) {
+            return {
+                module: TokensModule,
+                imports: [...(options.imports ?? [])],
+                providers: [
+                    {
+                        provide: 'TOKENS_MODULE_OPTIONS',
+                        useFactory: options.useFactory,
+                        inject: options.inject,
+                    },
+                    {
+                        provide: REDIS_FOR_TOKENS,
+                        useFactory: async (opts: ISessionsModuleOptions) => {
+                            const redisAdapter = new RedisAdapter({
+                                uri: opts.redisUri,
+                            });
+                            await redisAdapter.connection.connect();
+                            return redisAdapter;
+                        },
+                        inject: ['TOKENS_MODULE_OPTIONS'],
+                    },
+                    TokensService,
+                ],
+                exports: [TokensService],
+            };
+        } else {
+            throw new Error('This module must be configured by "useFactory"');
+        }
+    }
+}
