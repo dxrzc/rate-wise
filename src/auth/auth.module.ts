25adac137a7f786348e70962e46373b65d9de916
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-26 11:13:34 -0500

    chore(tokens): import tokens module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,31 @@
+import { AuthConfigService } from 'src/config/services/auth.config.service';
+import { DbConfigService } from 'src/config/services/db.config.service';
 import { HashingService } from 'src/common/services/hashing.service';
 import { LoggingModule } from 'src/logging/logging.module';
+import { CommonModule } from 'src/common/common.module';
+import { TokensModule } from 'src/tokens/tokens.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { Module } from '@nestjs/common';
-import { CommonModule } from 'src/common/common.module';
 
 @Module({
-    imports: [UsersModule, CommonModule, LoggingModule],
+    imports: [
+        UsersModule,
+        CommonModule,
+        LoggingModule,
+        TokensModule.forFeature({
+            inject: [DbConfigService, AuthConfigService],
+            useFactory: (
+                dbConfig: DbConfigService,
+                authConfig: AuthConfigService,
+            ) => ({
+                tokenSecret: authConfig.emailTokenSecret,
+                expiresIn: authConfig.emailTokenExpTime,
+                redisUri: dbConfig.redisAuthUri,
+            }),
+        }),
+    ],
     providers: [AuthResolver, AuthService, HashingService],
 })
 export class AuthModule {}
