87cda51a490c22368d0b89ca542467e14d237fa6
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-25 19:55:55 -0500

    refactor(sessions): use sess module options constant in sessions module

--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -1,6 +1,7 @@
 import { ISessionsModuleOptions } from './interface/sessions-module-options.interface';
 import { SessionMiddlewareFactory } from './middlewares/session.middleware.factory';
 import { REDIS_SESSIONS_CLIENT } from './constants/redis-sess-client.token.constant';
+import { SESS_MODULE_OPTS } from './constants/sess-module-opts.token.constant';
 import { RedisAdapter } from 'src/common/redis/redis.adapter';
 import { LoggingModule } from 'src/logging/logging.module';
 import { deleteSession } from './functions/delete-session';
@@ -24,7 +25,7 @@ export class SessionsModule {
                 imports: [LoggingModule, ...(options.imports ?? [])],
                 providers: [
                     {
-                        provide: 'SESS_MODULE_OPTS',
+                        provide: SESS_MODULE_OPTS,
                         useFactory: options.useFactory,
                         inject: options.inject,
                     },
@@ -44,7 +45,7 @@ export class SessionsModule {
                             await redisAdapter.connection.connect();
                             return redisAdapter;
                         },
-                        inject: ['SESS_MODULE_OPTS'],
+                        inject: [SESS_MODULE_OPTS],
                     },
                     SessionMiddlewareFactory,
                     SessionsService,
