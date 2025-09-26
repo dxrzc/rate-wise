e2a8d9c406c42247d1825c99d06f215f2f12af20
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-25 20:00:21 -0500

    refactor(sessions): use sess module options in sessions middleware factory classs

--- src/sessions/middlewares/session.middleware.factory.ts
+++ src/sessions/middlewares/session.middleware.factory.ts
@@ -1,36 +1,37 @@
+import { ISessionsModuleOptions } from '../interface/sessions-module-options.interface';
+import { REDIS_SESSIONS_CLIENT } from '../constants/redis-sess-client.token.constant';
+import { SESS_MODULE_OPTS } from '../constants/sess-module-opts.token.constant';
 import { ServerConfigService } from 'src/config/services/server.config.service';
-import { AuthConfigService } from 'src/config/services/auth.config.service';
+import { RedisAdapter } from 'src/common/redis/redis.adapter';
 import { Environment } from 'src/common/enum/environment.enum';
 import { Inject, Injectable } from '@nestjs/common';
 import * as session from 'express-session';
 import { RedisStore } from 'connect-redis';
-import { RedisAdapter } from 'src/common/redis/redis.adapter';
-import { REDIS_SESSIONS_CLIENT } from '../constants/redis-sess-client.token.constant';
 
 @Injectable()
 export class SessionMiddlewareFactory {
     constructor(
+        private readonly serverConfig: ServerConfigService,
         @Inject(REDIS_SESSIONS_CLIENT)
         private readonly redis: RedisAdapter,
-        private readonly authConfig: AuthConfigService,
-        private readonly serverConfig: ServerConfigService,
+        @Inject(SESS_MODULE_OPTS)
+        private readonly sessOpts: ISessionsModuleOptions,
     ) {}
 
     create() {
         const middleware = session({
-            name: this.authConfig.sessCookieName,
+            name: this.sessOpts.cookieName,
             resave: false,
             saveUninitialized: false,
-            secret: this.authConfig.sessCookieSecret,
+            secret: this.sessOpts.cookieSecret,
             unset: 'destroy',
             rolling: true,
             cookie: {
                 httpOnly: true,
-                maxAge: this.authConfig.sessCookieMaxAgeMs,
+                maxAge: this.sessOpts.cookieMaxAgeMs,
                 secure: this.serverConfig.env === Environment.PRODUCTION,
             },
             store: new RedisStore({
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                 client: this.redis.client,
                 prefix: 'session:',
             }),
