126ef31b7274503f1b3bf7e373dff974be89b609
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-28 22:46:45 -0500

    chore(auth): remove tokens module import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,31 +1,13 @@
-import { AuthConfigService } from 'src/config/services/auth.config.service';
-import { DbConfigService } from 'src/config/services/db.config.service';
 import { HashingService } from 'src/common/services/hashing.service';
 import { LoggingModule } from 'src/logging/logging.module';
 import { CommonModule } from 'src/common/common.module';
-import { TokensModule } from 'src/tokens/tokens.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [
-        UsersModule,
-        CommonModule,
-        LoggingModule,
-        TokensModule.forFeature({
-            inject: [DbConfigService, AuthConfigService],
-            useFactory: (
-                dbConfig: DbConfigService,
-                authConfig: AuthConfigService,
-            ) => ({
-                tokenSecret: authConfig.emailTokenSecret,
-                expiresIn: authConfig.emailTokenExpTime,
-                redisUri: dbConfig.redisAuthUri,
-            }),
-        }),
-    ],
+    imports: [UsersModule, CommonModule, LoggingModule],
     providers: [AuthResolver, AuthService, HashingService],
 })
 export class AuthModule {}
