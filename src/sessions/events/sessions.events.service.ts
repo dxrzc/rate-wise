18912766c768014f1ae952aa1b13acc2359b7255
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-29 19:34:03 -0500

    chore(sessions): handle delete/expired sessions in a specific class

---
+++ src/sessions/events/sessions.events.service.ts
@@ -0,0 +1,48 @@
+import { userAndSessionRelationKey } from '../functions/user-session-relation-key';
+import { userSessionsSetKey } from '../functions/sessions-index-key';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { REDIS_AUTH } from 'src/redis/constants/redis.constants';
+import { RedisService } from 'src/redis/redis.service';
+
+@Injectable()
+export class SessionsEventsService implements OnModuleInit {
+    constructor(
+        @Inject(REDIS_AUTH)
+        private readonly redisService: RedisService,
+    ) {}
+
+    async onModuleInit() {
+        const listener = (key: string) => this.handleRemovedSession(key);
+        await Promise.all([
+            this.redisService.connection.addSubscriber(
+                '__keyevent@0__:del',
+                listener,
+            ),
+            this.redisService.connection.addSubscriber(
+                '__keyevent@0__:expired',
+                listener,
+            ),
+        ]);
+    }
+
+    async handleRemovedSession(key: string): Promise<void> {
+        if (!key.startsWith('session')) return;
+
+        const sessionId = key.split(':').at(1);
+        if (!sessionId) throw new Error(`Invalid session key format`);
+
+        const userSessionRelationKey = userAndSessionRelationKey(sessionId);
+        const userId = await this.redisService.get<string>(
+            userSessionRelationKey,
+        );
+
+        if (userId) {
+            await Promise.all([
+                // remove session-userId relation record
+                this.redisService.delete(userSessionRelationKey),
+                // remove from set
+                this.redisService.setRem(userSessionsSetKey(userId), sessionId),
+            ]);
+        }
+    }
+}
