2013a08d279e1dcdf21397259e3aae44de4e342d
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-29 19:50:00 -0500

    fix(sessions): configure missing SessionsEventsService as provider

--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -2,11 +2,16 @@ import { SessionMiddlewareFactory } from './middlewares/session.middleware.facto
 import { LoggingModule } from 'src/logging/logging.module';
 import { SessionsService } from './sessions.service';
 import { Global, Module } from '@nestjs/common';
+import { SessionsEventsService } from './events/sessions.events.service';
 
 @Global()
 @Module({
     imports: [LoggingModule],
-    providers: [SessionsService, SessionMiddlewareFactory],
+    providers: [
+        SessionsService,
+        SessionMiddlewareFactory,
+        SessionsEventsService,
+    ],
     exports: [SessionsService, SessionMiddlewareFactory],
 })
 export class SessionsModule {}
