ae8550d8a74ea8c1fd1a094f929f82cbc54d619b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-30 10:44:56 -0500

    chore(types): remove unused type

--- src/common/types/modules/config-async-module.type.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigurableModuleAsyncOptions, Provider } from '@nestjs/common';
-
-export type AsyncConfigModule<T> = ConfigurableModuleAsyncOptions<T> & {
-    extraProviders?: Provider[];
-};
