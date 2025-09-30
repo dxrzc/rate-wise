df3a8417444c454be969cd0801162ae46af9caf7
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-29 19:40:37 -0500

    chore(redis): add client getter in redis service

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -13,8 +13,12 @@ import { RedisConnection } from './redis.connection';
 export class RedisService {
     private readonly _connection: RedisConnection;
 
-    constructor(private readonly client: any) {
-        this._connection = new RedisConnection(client);
+    constructor(private readonly _client: any) {
+        this._connection = new RedisConnection(_client);
+    }
+
+    get client() {
+        return this._client;
     }
 
     get connection() {
@@ -25,42 +29,42 @@ export class RedisService {
      * Adds a member to a Redis set.
      */
     async setAdd(key: string, member: string): Promise<void> {
-        await this.client.sAdd(key, member);
+        await this._client.sAdd(key, member);
     }
 
     /**
      * Removes a member from a Redis set.
      */
     async setRem(key: string, member: string): Promise<void> {
-        await this.client.sRem(key, member);
+        await this._client.sRem(key, member);
     }
 
     /**
      * Checks if a member exists in a Redis set.
      */
     async setIsMember(key: string, member: string): Promise<boolean> {
-        return (await this.client.sIsMember(key, member)) === 1;
+        return (await this._client.sIsMember(key, member)) === 1;
     }
 
     /**
      * Retrieves all members of a Redis set.
      */
     async setMembers(key: string): Promise<string[]> {
-        return await this.client.sMembers(key);
+        return await this._client.sMembers(key);
     }
 
     /**
      * Returns the number of members in a Redis set.
      */
     async setSize(key: string): Promise<number> {
-        return await this.client.sCard(key);
+        return await this._client.sCard(key);
     }
 
     /**
      * Deletes a key from Redis.
      */
     async delete(key: string): Promise<void> {
-        await this.client.del(key);
+        await this._client.del(key);
     }
 
     /**
@@ -75,9 +79,11 @@ export class RedisService {
             data = JSON.stringify(data);
         }
         if (expTimeSeconds) {
-            await this.client.set(key, data.toString(), { EX: expTimeSeconds });
+            await this._client.set(key, data.toString(), {
+                EX: expTimeSeconds,
+            });
         } else {
-            await this.client.set(key, data.toString());
+            await this._client.set(key, data.toString());
         }
     }
 
@@ -85,7 +91,7 @@ export class RedisService {
      * Retrieves a value from Redis.
      */
     async get<T>(key: string): Promise<T | null> {
-        const data = await this.client.get(key);
+        const data = await this._client.get(key);
         if (data) {
             try {
                 return JSON.parse(data) as T;
@@ -101,7 +107,7 @@ export class RedisService {
      * Performs a Redis transaction (multi/exec).
      */
     transaction() {
-        const multi = this.client.multi();
+        const multi = this._client.multi();
 
         const wrapper = {
             setAdd: (key: string, member: string) => {
