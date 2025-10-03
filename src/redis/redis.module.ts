1c82f1d2304bc93e7d2ed370280b0766521f8046
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-02 22:46:42 -0500

    refactor(redis): change to forRootAsyncImport

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,41 +1,64 @@
-/**
- * node-redis has serious performance issues when working with typescript
- * so do not use the "RedisClientType" type.
- */
-
-import { DbConfigService } from 'src/config/services/db.config.service';
-import { Global, Module, OnApplicationShutdown } from '@nestjs/common';
+import { IRedisOptions } from './interface/redis.options.interface';
 import { REDIS_AUTH } from './constants/redis.constants';
 import { RedisService } from './redis.service';
 import { createClient } from '@redis/client';
 import { ModuleRef } from '@nestjs/core';
+import {
+    ConfigurableModuleAsyncOptions,
+    DynamicModule,
+    Global,
+    Module,
+    OnApplicationShutdown,
+} from '@nestjs/common';
 
 @Global()
-@Module({
-    providers: [
-        {
+@Module({})
+export class RedisModule implements OnApplicationShutdown {
+    private static readonly optionsProviderToken = 'REDIS_MODULE_OPTIONS';
+
+    constructor(private moduleRef: ModuleRef) {}
+
+    async onApplicationShutdown() {
+        const redisAuth = this.moduleRef.get<RedisService>(REDIS_AUTH);
+        await Promise.all([redisAuth.connection.disconnect()]);
+    }
+
+    private static provideRedisForAuth() {
+        return {
             provide: REDIS_AUTH,
-            inject: [DbConfigService],
+            inject: [this.optionsProviderToken],
             useFactory: async (
-                dbConfig: DbConfigService,
+                options: IRedisOptions,
             ): Promise<RedisService> => {
                 const client = createClient({
-                    url: dbConfig.redisAuthUri,
+                    url: options.redisAuth,
                 });
                 const redisService = new RedisService(client);
                 await redisService.connection.connect();
                 await client.configSet('notify-keyspace-events', 'ExgK');
                 return redisService;
             },
-        },
-    ],
-    exports: [REDIS_AUTH],
-})
-export class RedisModule implements OnApplicationShutdown {
-    constructor(private moduleRef: ModuleRef) {}
+        };
+    }
 
-    async onApplicationShutdown() {
-        const redisAuth = this.moduleRef.get<RedisService>(REDIS_AUTH);
-        await Promise.all([redisAuth.connection.disconnect()]);
+    static forRootAsync(
+        options: ConfigurableModuleAsyncOptions<IRedisOptions>,
+    ): DynamicModule {
+        if (!options.useFactory)
+            throw new Error('RedisModule requires useFactory option');
+
+        return {
+            module: RedisModule,
+            imports: [...(options.imports || [])],
+            providers: [
+                {
+                    provide: this.optionsProviderToken,
+                    useFactory: options.useFactory,
+                    inject: options.inject || [],
+                },
+                this.provideRedisForAuth(),
+            ],
+            exports: [REDIS_AUTH],
+        };
     }
 }
