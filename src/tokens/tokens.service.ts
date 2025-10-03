13fd0c61e3c073d715b41a070269d9193e3b6f5e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-03 11:42:52 -0500

    chore(tokens): add token service verify and generate methods

--- src/tokens/tokens.service.ts
+++ src/tokens/tokens.service.ts
@@ -1,9 +1,26 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { REDIS_AUTH } from 'src/redis/constants/redis.constants';
 import { RedisService } from 'src/redis/redis.service';
 import { ITokensOptions } from './interfaces/tokens.options.interface';
+import { isSubset } from 'src/common/functions/utils/is-subset.util';
+import { JsonWebTokenError, JwtService } from '@nestjs/jwt';
 import { TOKENS_OPTIONS } from './constants/tokens.constants';
+import { JwtPurpose } from 'src/common/enum/jwt.purpose.enum';
+import {
+    InvalidDataInToken,
+    InvalidToken,
+    InvalidTokenPurpose,
+} from './errors/invalid-token.error';
+import { v4 as uuidv4 } from 'uuid';
+import {
+    Inject,
+    Injectable,
+    InternalServerErrorException,
+} from '@nestjs/common';
+
+type TokenPayload = {
+    purpose: JwtPurpose;
+    [prop: string]: any;
+};
 
 @Injectable()
 export class TokensService {
@@ -11,11 +28,43 @@ export class TokensService {
         @Inject(REDIS_AUTH) private readonly redisService: RedisService,
         @Inject(TOKENS_OPTIONS) private readonly tokensOpts: ITokensOptions,
         private readonly jwtService: JwtService,
-    ) {}
+    ) {
+        this.tokensOpts.dataInToken.push('purpose', 'jti');
+    }
+
+    private verifyTokenOrThrow<T extends TokenPayload>(token: string): T {
+        try {
+            return this.jwtService.verify<T>(token);
+        } catch (error) {
+            if (error instanceof JsonWebTokenError) {
+                throw new InvalidToken(error.message);
+            }
+            throw new InternalServerErrorException(error);
+        }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async verify<T extends TokenPayload>(token: string): Promise<T> {
+        // JwtModule verification
+        const payload = this.verifyTokenOrThrow<T>(token);
+
+        // expected custom data in token (jti, purpose, tokenOpts.dataInToken)
+        if (!isSubset(Object.keys(payload), this.tokensOpts.dataInToken))
+            throw new InvalidDataInToken();
+
+        // correct purpose
+        if (payload.purpose !== this.tokensOpts.purpose)
+            throw new InvalidTokenPurpose();
+
+        // is not blacklisted...
+
+        return payload;
+    }
 
     generate(payload: object): string {
         const token = this.jwtService.sign({
-            purpose: this.tokensOpts.type,
+            purpose: this.tokensOpts.purpose,
+            jti: uuidv4(),
             ...payload,
         });
         return token;
