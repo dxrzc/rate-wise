b705df5e1ba62b6a6d35c18b2a1ca87cfb8573ae
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-04 17:43:26 -0500

    refactor(logging): use forRootAsync in LoggingModule

--- src/logging/logging.module.ts
+++ src/logging/logging.module.ts
@@ -1,8 +1,27 @@
+import { FactoryConfigModule } from 'src/common/types/modules/factory-config.module.type';
 import { HttpLoggerService } from './http/http-logger.service';
-import { Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { ILoggingOptions } from './interfaces/logging.options.interface';
+import { LOGGING_OPTIONS } from './constants/logging.options.constants';
 
-@Module({
-    providers: [HttpLoggerService],
-    exports: [HttpLoggerService],
-})
-export class LoggingModule {}
+@Global()
+@Module({})
+export class LoggingModule {
+    static forRootAsync(
+        options: FactoryConfigModule<ILoggingOptions>,
+    ): DynamicModule {
+        return {
+            module: LoggingModule,
+            imports: [...(options.imports || [])],
+            providers: [
+                HttpLoggerService,
+                {
+                    provide: LOGGING_OPTIONS,
+                    useFactory: options.useFactory,
+                    inject: options.inject,
+                },
+            ],
+            exports: [HttpLoggerService],
+        };
+    }
+}
