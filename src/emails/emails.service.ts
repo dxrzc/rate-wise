0869dea5eeb61f01368201d6ec85ae7c93966edf
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-21 13:25:35 -0500

    chore(emails): use emails queue options to set retry attemps

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,24 +1,30 @@
 import { InjectQueue } from '@nestjs/bullmq';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Queue } from 'bullmq';
-import { EMAILS_QUEUE } from './constants/emails.constants';
 import { IDeduplicationConfig } from './interface/deduplication.config.interface';
 import { IEmailInfo } from './interface/email-info.interface';
+import { IEmailsQueueOptions } from './interface/emails-queue.options.interface';
+import {
+    EMAILS_QUEUE,
+    EMAILS_QUEUE_OPTIONS,
+} from './constants/emails.constants';
 
 @Injectable()
 export class EmailsService {
     constructor(
         @InjectQueue(EMAILS_QUEUE)
         private emailsQueue: Queue,
+        @Inject(EMAILS_QUEUE_OPTIONS)
+        private queueOpts: IEmailsQueueOptions,
     ) {}
 
     async sendEmail(data: IEmailInfo, deduplication: IDeduplicationConfig) {
         // duplicated jobs added during the deduplication delay period are ignored
         await this.emailsQueue.add(`email`, data, {
+            attempts: this.queueOpts.retryAttempts,
             removeOnComplete: true,
             removeOnFail: true,
             deduplication,
-            attempts: 3,
             backoff: {
                 type: 'exponential',
                 delay: 1000,
