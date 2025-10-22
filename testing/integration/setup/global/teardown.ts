704a8e4b28fb6ac88c83092b3f6078689e5c22b6
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-22 08:46:47 -0500

    fix(tests): remove await from global teardown in integration

--- testing/integration/setup/global/teardown.ts
+++ testing/integration/setup/global/teardown.ts
@@ -4,9 +4,9 @@ import { join } from 'path';
 export default async function () {
     try {
         await Promise.all([
-            await fs.rm(join(__dirname, 'containers/postgres-uri.txt')),
-            await fs.rm(join(__dirname, 'containers/redis-auth-uri.txt')),
-            await fs.rm(join(__dirname, 'containers/redis-queues-uri.txt')),
+            fs.rm(join(__dirname, 'containers/postgres-uri.txt')),
+            fs.rm(join(__dirname, 'containers/redis-auth-uri.txt')),
+            fs.rm(join(__dirname, 'containers/redis-queues-uri.txt')),
         ]);
     } catch (error) {
         console.error(error);
