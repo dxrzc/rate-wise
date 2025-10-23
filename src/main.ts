1756329088cea1a915171e37159db2001bdf9d60
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-22 20:03:45 -0500

    refactor(bootstrap): register system logger as app logger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { NestFactory } from '@nestjs/core';
 import { SystemLoggerService } from './system-logger/system-logger.service';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        bufferLogs: true,
+    });
+    const systemLogger = app.get(SystemLoggerService);
+    app.useLogger(systemLogger);
+
     app.enableShutdownHooks();
     const serverConfig = app.get(ServerConfigService);
     await app.listen(serverConfig.port);
-    const systemLogger = app.get(SystemLoggerService);
-    systemLogger.log(`Running in ${serverConfig.env.toUpperCase()} mode`);
+    systemLogger.log(`Running in ${serverConfig.env} mode`, 'Bootstrap');
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
