9a0499d3ea4066c5ef6bfd7463fae6db41b4e398
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-23 13:36:39 -0500

    feat(app): handle uncaughtException and unhandledRejection signals

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,60 @@
-import { ServerConfigService } from './config/services/server.config.service';
-import { AppModule } from './app/app.module';
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SystemLoggerService } from './system-logger/system-logger.service';
+import { AppModule } from './app/app.module';
+import { SystemLogger } from './common/logging/system.logger';
+import { ServerConfigService } from './config/services/server.config.service';
 
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        bufferLogs: true,
-    });
-    const systemLogger = app.get(SystemLoggerService);
-    app.useLogger(systemLogger);
+let app: INestApplication | undefined;
 
-    app.enableShutdownHooks();
-    const serverConfig = app.get(ServerConfigService);
-    await app.listen(serverConfig.port);
-    systemLogger.log(`Running in ${serverConfig.env} mode`, 'Bootstrap');
+function tryToCloseApp(app: INestApplication) {
+    app.close()
+        .finally(() => process.exit(1))
+        .catch((err: Error) => {
+            SystemLogger.getInstance().error(
+                `Error closing nest application: ${err.message}`,
+                err.stack,
+            );
+        });
 }
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+process.on('uncaughtException', ({ message, stack }: Error) => {
+    SystemLogger.getInstance().error(`uncaughtException: ${message}`, stack);
+    if (app) {
+        tryToCloseApp(app);
+        return;
+    }
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason: unknown) => {
+    const logger = SystemLogger.getInstance();
+    if (reason instanceof Error) {
+        logger.error(`unhandledRejection: ${reason.message}`, reason.stack);
+    } else {
+        logger.error(`unhandledRejection: ${String(reason)}`);
+    }
+    if (app) {
+        tryToCloseApp(app);
+        return;
+    }
+    process.exit(1);
+});
+
+async function bootstrap() {
+    app = await NestFactory.create(AppModule, {
+        bufferLogs: true,
+    });
+    app.useLogger(SystemLogger.getInstance());
+    app.enableShutdownHooks();
+
+    const serverConfig = app.get(ServerConfigService);
+    await app.listen(serverConfig.port);
+    SystemLogger.getInstance().log(
+        `Running in ${serverConfig.env} mode`,
+        'Bootstrap',
+    );
+}
+
+bootstrap().catch((error) => {
+    throw error;
+});
