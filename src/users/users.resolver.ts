fb1e854169280998804ae3bed1cf357994942afa
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-27 12:19:52 -0500

    chore(users-api): add throttler decorators in findOneById and findAll endpoints

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,14 +1,19 @@
-import { UsersService } from './users.service';
-import { UserModel } from './models/user.model';
 import { Args, ID, Query, Resolver } from '@nestjs/graphql';
-import { UserPaginationModel } from './models/pagination.model';
+import {
+    BalancedThrottle,
+    RelaxedThrottle,
+} from 'src/common/decorators/throttling.decorator';
 import { PaginationArgs } from 'src/common/dtos/args/pagination.args';
 import { IPaginatedType } from 'src/common/interfaces/pagination/paginated-type.interface';
+import { UserPaginationModel } from './models/pagination.model';
+import { UserModel } from './models/user.model';
+import { UsersService } from './users.service';
 
 @Resolver(() => UserModel)
 export class UsersResolver {
     constructor(private readonly userService: UsersService) {}
 
+    @RelaxedThrottle()
     @Query(() => UserModel, { name: 'findUserById' })
     async findOneById(
         @Args('user_id', { type: () => ID }) id: string,
@@ -16,6 +21,7 @@ export class UsersResolver {
         return await this.userService.findOneByIdOrThrow(id);
     }
 
+    @BalancedThrottle()
     @Query(() => UserPaginationModel, { name: 'users' })
     async findAll(
         @Args() paginationArgs: PaginationArgs,
