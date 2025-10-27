45c623371b6083e80050c83540f3e2970ee38c4b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-27 18:12:52 -0500

    fix(auth): remove email max length validation in sign-in input type

--- src/auth/dtos/sign-in.input.ts
+++ src/auth/dtos/sign-in.input.ts
@@ -1,12 +1,10 @@
-import { IsEmail, IsString, MaxLength } from 'class-validator';
-import { AUTH_LIMITS } from '../constants/auth.constants';
+import { IsEmail, IsString } from 'class-validator';
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class SignInInput {
     @IsString()
     @IsEmail()
-    @MaxLength(AUTH_LIMITS.EMAIL.MAX)
     @Field(() => String)
     email!: string;
 
