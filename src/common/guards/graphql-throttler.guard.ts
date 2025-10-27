3641d408017bc3cd51060f15f96c8e503061e9df
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-26 21:19:11 -0500

    chore(throttler): add user id as tracker and http logging when throwing exception in gql throttler

--- src/common/guards/graphql-throttler.guard.ts
+++ src/common/guards/graphql-throttler.guard.ts
@@ -1,19 +1,47 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { ThrottlerGuard } from '@nestjs/throttler';
-import { IGraphQLContext } from '../interfaces/graphql/graphql-context.interface';
+import {
+    ThrottlerGuard,
+    ThrottlerLimitDetail,
+    ThrottlerOptions,
+    ThrottlerStorage,
+} from '@nestjs/throttler';
+import { RequestContext } from 'src/auth/types/request-context.type';
+import { HttpLoggerService } from 'src/http-logger/http-logger.service';
 import { GqlHttpError } from '../errors/graphql-http.error';
+import { IGraphQLContext } from '../interfaces/graphql/graphql-context.interface';
 import { COMMON_MESSAGES } from '../messages/common.messages';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
+    constructor(
+        options: ThrottlerOptions[],
+        storageService: ThrottlerStorage,
+        reflector: Reflector,
+        private readonly logger: HttpLoggerService,
+    ) {
+        super(options, storageService, reflector);
+    }
+
+    // Uses user ID as tracker if authenticated, otherwise falls back to IP address
+    async getTracker(req: Record<string, any>): Promise<string> {
+        const typedReq = req as RequestContext;
+        return typedReq.user ? typedReq.user.id : typedReq.ip!;
+    }
+
     getRequestResponse(context: ExecutionContext) {
         const gqlCtx = GqlExecutionContext.create(context);
         const ctx = gqlCtx.getContext<IGraphQLContext>();
         return { req: ctx.req, res: ctx.res };
     }
 
-    protected throwThrottlingException(): Promise<void> {
+    throwThrottlingException(
+        context: ExecutionContext,
+        { limit, ttl }: ThrottlerLimitDetail,
+    ): Promise<void> {
+        this.logger.error(`More than ${limit} requests in ${ttl}ms`);
         throw GqlHttpError.TooManyRequests(COMMON_MESSAGES.TOO_MANY_REQUESTS);
     }
 }
