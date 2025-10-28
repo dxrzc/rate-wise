61ed874f2262a3f2123e19efd5c745a617d68c60
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-28 14:28:38 -0500

    chore(auth): import token module to provide ACCOUNT_VERIFICATION_TOKEN

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,10 @@ import { Module } from '@nestjs/common';
 import { HttpLoggerModule } from 'src/http-logger/http-logger.module';
 import { EmailsModule } from 'src/emails/emails.module';
 import { AuthNotifications } from './notifications/auth.notifications';
+import { TokensModule } from 'src/tokens/tokens.module';
+import { JwtPurpose } from 'src/tokens/enums/jwt-purpose.enum';
+import { ACCOUNT_VERIFICATION_TOKEN } from './constants/tokens.provider.constant';
+import { AuthController } from './auth.controller';
 
 @Module({
     imports: [
@@ -20,7 +24,17 @@ import { AuthNotifications } from './notifications/auth.notifications';
                 },
             }),
         }),
+        TokensModule.forFeatureAsync({
+            provide: ACCOUNT_VERIFICATION_TOKEN,
+            useFactory: () => ({
+                secret: '123xdd',
+                expiresIn: '10m',
+                purpose: JwtPurpose.ACCOUNT_VERIFICATION,
+                dataInToken: ['id'],
+            }),
+        }),
     ],
+    controllers: [AuthController],
     providers: [AuthResolver, AuthService, HashingService, AuthNotifications],
 })
 export class AuthModule {}
