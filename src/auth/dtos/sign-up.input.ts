558e81dd147a804f2114ffa3f883adaabf2ebbb5
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-27 20:06:02 -0500

    fix(auth): add constraints descriptions to SignUpInput fields

--- src/auth/dtos/sign-up.input.ts
+++ src/auth/dtos/sign-up.input.ts
@@ -7,18 +7,34 @@ export class SignUpInput {
     @IsString()
     @MinLength(AUTH_LIMITS.USERNAME.MIN)
     @MaxLength(AUTH_LIMITS.USERNAME.MAX)
-    @Field(() => String)
+    @Field(() => String, {
+        description: `        
+        **Constraints:**
+        - Length between ${AUTH_LIMITS.USERNAME.MIN} and ${AUTH_LIMITS.USERNAME.MAX} characters.
+        `,
+    })
     username!: string;
 
     @IsString()
     @IsEmail()
     @MaxLength(AUTH_LIMITS.EMAIL.MAX)
-    @Field(() => String)
+    @Field(() => String, {
+        description: `
+        **Constraints:**
+        - Valid email format.
+        - Maximum length of ${AUTH_LIMITS.EMAIL.MAX} characters.
+        `,
+    })
     email!: string;
 
     @IsString()
     @MinLength(AUTH_LIMITS.PASSWORD.MIN)
     @MaxLength(AUTH_LIMITS.PASSWORD.MAX)
-    @Field(() => String)
+    @Field(() => String, {
+        description: `        
+        **Constraints:**
+        - Length between ${AUTH_LIMITS.PASSWORD.MIN} and ${AUTH_LIMITS.PASSWORD.MAX} characters.
+        `,
+    })
     password!: string;
 }
