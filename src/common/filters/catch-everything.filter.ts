047e2ebb3fc84fbaca976415efdfe85594c0d783
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-28 14:08:28 -0500

    chore(errors): expand catch-everything filter

--- src/common/filters/catch-everything.filter.ts
+++ src/common/filters/catch-everything.filter.ts
@@ -1,16 +1,51 @@
-import { Catch, ExceptionFilter } from '@nestjs/common';
-import { SystemLogger } from '../logging/system.logger';
+import {
+    ArgumentsHost,
+    Catch,
+    ExceptionFilter,
+    HttpException,
+    HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { GraphQLError } from 'graphql';
+import { SystemLogger } from '../logging/system.logger';
+import { GqlContextType } from '@nestjs/graphql';
 
-@Catch(Error)
-export class CatchEverythingFilter implements ExceptionFilter {
-    catch(exception: Error) {
-        if (exception instanceof GraphQLError) {
-            // already handled
-            return exception;
-        }
-        // Unknown errors
+function logException(exception: unknown) {
+    if (exception instanceof Error)
         SystemLogger.getInstance().error(exception.message, exception.stack);
+    else SystemLogger.getInstance().error(exception);
+}
+
+@Catch()
+export class CatchEverythingFilter implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+        switch (host.getType<GqlContextType>()) {
+            case 'http': {
+                const ctx = host.switchToHttp();
+                const response = ctx.getResponse<Response>();
+                if (exception instanceof HttpException) {
+                    response
+                        .status(exception.getStatus())
+                        .json(exception.getResponse());
+                    return;
+                }
+                const statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+                response
+                    .status(statusCode)
+                    .json({ error: 'Internal server error', statusCode });
+                logException(exception);
+                break;
+            }
+            case 'graphql': {
+                if (!(exception instanceof GraphQLError)) {
+                    logException(exception);
+                }
+                break;
+            }
+            default: {
+                logException(exception);
+            }
+        }
         return exception;
     }
 }
