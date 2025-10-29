0700c0ad8fc26de71a2cf00d15c8a6413bf7eef5
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-29 13:42:11 -0500

    chore(auth): add new messages

--- src/auth/messages/auth.messages.ts
+++ src/auth/messages/auth.messages.ts
@@ -2,4 +2,8 @@ export const AUTH_MESSAGES = {
     MAX_SESSIONS_REACHED: 'Number of active sessions exceeded.',
     INVALID_CREDENTIALS: 'Invalid credentials',
     UNAUTHORIZED: 'Authentication required',
+    INVALID_TOKEN: 'Invalid or expired token',
+    INVALID_URL: 'Invalid url',
+    ACCOUNT_ALREADY_VERIFIED: 'Account is already verified',
+    ACCOUNT_SUSPENDED: 'Account is suspended',
 };
