b6adbf3bdb372b92bde5b6e8107b7e5de1dc55c5
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-30 18:26:27 -0500

    test(auth): add test for handling TOO MANY REQUESTS in requestAccountVerification

--- testing/integration/specs/auth/api/request-account-verification.test.ts
+++ testing/integration/specs/auth/api/request-account-verification.test.ts
@@ -1,11 +1,14 @@
 import { requestAccountVerification } from '@commontestutils/operations/auth/request-account-verification.operation';
+import { faker } from '@faker-js/faker/.';
 import {
     createActiveUser,
     createUser,
 } from '@integration/utils/create-user.util';
 import { testKit } from '@integration/utils/test-kit.util';
 import { AUTH_MESSAGES } from 'src/auth/messages/auth.messages';
+import { THROTTLE_CONFIG } from 'src/common/constants/throttle.config.constants';
 import { Code } from 'src/common/enum/code.enum';
+import { COMMON_MESSAGES } from 'src/common/messages/common.messages';
 
 describe('requestAccountVerification', () => {
     describe('Target account is already verified', () => {
@@ -29,6 +32,23 @@ describe('requestAccountVerification', () => {
                 .send(requestAccountVerification());
             expect(res).notToFail();
             await expect(email).emailSentToThisAddress();
+        });
+    });
+
+    describe(`More than ${THROTTLE_CONFIG.ULTRA_CRITICAL.limit} attemps in ${THROTTLE_CONFIG.ULTRA_CRITICAL.ttl / 1000}s from the same ip`, () => {
+        test('should return TOO MANY REQUESTS code and message', async () => {
+            const ip = faker.internet.ip();
+            for (let i = 0; i < THROTTLE_CONFIG.ULTRA_CRITICAL.limit; i++)
+                await testKit.request
+                    .set('X-Forwarded-For', ip)
+                    .send(requestAccountVerification());
+            const res = await testKit.request
+                .set('X-Forwarded-For', ip)
+                .send(requestAccountVerification());
+            expect(res).toFailWith(
+                Code.TOO_MANY_REQUESTS,
+                COMMON_MESSAGES.TOO_MANY_REQUESTS,
+            );
         });
     });
 });
