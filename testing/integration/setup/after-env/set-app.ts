0cd81473834b3f60f01a29061b04055be5a7216a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-30 18:52:19 -0500

    feat(tests): add restClient to testKit for REST requests with random IP

--- testing/integration/setup/after-env/set-app.ts
+++ testing/integration/setup/after-env/set-app.ts
@@ -51,6 +51,31 @@ beforeAll(async () => {
                     .post('/graphql')
                     .set('X-Forwarded-For', faker.internet.ip()),
         });
+        // Returns a new a REST request coming from a random ip address
+        // on each call
+        Object.defineProperty(testKit, 'restClient', {
+            get: () => {
+                const client = request(testKit.app.getHttpServer());
+                return {
+                    get: (url: string) =>
+                        client
+                            .get(url)
+                            .set('X-Forwarded-For', faker.internet.ip()),
+                    post: (url: string) =>
+                        client
+                            .post(url)
+                            .set('X-Forwarded-For', faker.internet.ip()),
+                    put: (url: string) =>
+                        client
+                            .put(url)
+                            .set('X-Forwarded-For', faker.internet.ip()),
+                    delete: (url: string) =>
+                        client
+                            .delete(url)
+                            .set('X-Forwarded-For', faker.internet.ip()),
+                };
+            },
+        });
     } catch (error) {
         console.error(error);
         if (nestApp) await nestApp.close();
