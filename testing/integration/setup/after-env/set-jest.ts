3797e0d56d26d915da45c4989cb1bea61a086568
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-01 16:00:58 -0500

    chore(tests): move not-to-fail matcher into common utils and add global.d.ts

--- testing/integration/setup/after-env/set-jest.ts
+++ testing/integration/setup/after-env/set-jest.ts
@@ -1,6 +1,6 @@
 import { expect } from '@jest/globals';
 import { toContainCookie } from '@integration/custom/matchers/to-contain-cookie';
-import { notToFail } from '@integration/custom/matchers/not-to-fail';
+import { notToFail } from '@commontestutils/jest-matchers/not-to-fail';
 import { emailSentToThisAddress } from '@integration/custom/matchers/email-sent-to-address';
 import { toFailWith } from '@commontestutils/jest-matchers/to-fail-with';
 
