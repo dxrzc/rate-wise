60999e9da25081efcb12ebc64463a9048322331b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-01 15:00:09 -0500

    chore(errors): add INTERNAL_SERVER_ERROR common error message

--- src/common/messages/common.messages.ts
+++ src/common/messages/common.messages.ts
@@ -1,4 +1,5 @@
 export const COMMON_MESSAGES = {
     INVALID_INPUT: 'Input validation failed',
     TOO_MANY_REQUESTS: 'Too many requests, try again later',
+    INTERNAL_SERVER_ERROR: 'Internal server error',
 };
