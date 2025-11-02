563ac853e5de310bf2fb8869dd754633689be66c
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-02 17:19:27 -0500

    fix(components-test): register notToFail matcher in jest

--- testing/components/setup/setup-after-env.ts
+++ testing/components/setup/setup-after-env.ts
@@ -1,8 +1,10 @@
+import { notToFail } from '@commontestutils/jest-matchers/not-to-fail';
 import { toFailWith } from '@commontestutils/jest-matchers/to-fail-with';
 import { expect } from '@jest/globals';
 
 expect.extend({
     toFailWith,
+    notToFail,
 });
 
 beforeAll(() => {});
