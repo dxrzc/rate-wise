678869b676e4519ccf54f1713908fdb4fdf1a782
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-06 13:45:13 -0500

    docs: add missing JSDoc comments for IOperation interface and QueryFields type

--- testing/tools/gql-operations/interfaces/operation.interface.ts
+++ testing/tools/gql-operations/interfaces/operation.interface.ts
@@ -1,5 +1,9 @@
 import { QueryFields } from '../types/query-fields.type';
 
+/**
+ * @param InputType The input type class (e.g SignUpInput)
+ * @param ModelType The model type class (e.g UserModel)
+ */
 export interface IOperation<InputType = unknown, ModelType = any> {
     args: InputType;
     fields?: QueryFields<ModelType>[] | 'ALL';
