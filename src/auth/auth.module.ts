c7292fa4570c5871c652835d124fc74c269c22b4
dxrzc <diegorodriguezc.dev@gmail.com>
2025-11-09 17:29:49 -0500

    chore(auth): add todo in auth module file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -24,6 +24,7 @@ import { AuthController } from './auth.controller';
                 },
             }),
         }),
+        // TODO: use config service
         TokensModule.forFeatureAsync({
             provide: ACCOUNT_VERIFICATION_TOKEN,
             useFactory: () => ({
