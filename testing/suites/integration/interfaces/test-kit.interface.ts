ef8a70384fb83733f694aca7f24bfb083bd3287c
dxrzc <diegorodriguezc.dev@gmail.com>
2025-11-10 10:14:37 -0500

    feat(auth): integrate account deletion and verification token services into test kit

--- testing/suites/integration/interfaces/test-kit.interface.ts
+++ testing/suites/integration/interfaces/test-kit.interface.ts
@@ -7,6 +7,11 @@ import { AuthConfigService } from 'src/config/services/auth.config.service';
 import { Test } from 'supertest';
 import { RedisClientAdapter } from 'src/common/redis/redis.client.adapter';
 import { RestClient } from './rest-client.interface';
+import { TokensService } from 'src/tokens/tokens.service';
+import {
+    IAccDeletionTokenPayload,
+    IAccVerifTokenPayload,
+} from 'src/auth/interfaces/tokens-payload.interface';
 
 export interface ITestKit {
     app: INestApplication<App>;
@@ -17,6 +22,8 @@ export interface ITestKit {
     sessionsRedisClient: RedisClientAdapter;
     gqlClient: Test;
     restClient: RestClient;
+    accDeletionToken: TokensService<IAccDeletionTokenPayload>;
+    accVerifToken: TokensService<IAccVerifTokenPayload>;
     endpointsREST: {
         verifyAccount: string;
         deleteAccount: string;
