f0e4cf016564d4436583ffa374c25740b7898329
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-11 10:03:46 -0500

    refactor: auth module

--- src/auth/notifications/auth.notifications.ts
+++ src/auth/notifications/auth.notifications.ts
@@ -1,21 +1,17 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { AuthenticatedUser } from 'src/common/interfaces/user/authenticated-user.interface';
-import { ServerConfigService } from 'src/config/services/server.config.service';
-import { EmailsService } from 'src/emails/emails.service';
-import { TokensService } from 'src/tokens/tokens.service';
-import {
-    ACCOUNT_DELETION_TOKEN,
-    ACCOUNT_VERIFICATION_TOKEN,
-} from '../constants/tokens.provider.constant';
-import {
-    IAccDeletionTokenPayload,
-    IAccVerifTokenPayload,
-} from '../interfaces/tokens-payload.interface';
-import { verifyYourEmailHtml, verifyYourEmailPlainText } from '../pages/verify-your-email.page';
 import {
     verifyAccountDeletionHtml,
     verifyAccountDeletionPlainText,
 } from '../pages/verify-account-deletion.page';
+import {
+    ACCOUNT_DELETION_TOKEN,
+    ACCOUNT_VERIFICATION_TOKEN,
+} from '../constants/tokens.provider.constant';
+import { Inject, Injectable } from '@nestjs/common';
+import { AuthenticatedUser } from 'src/common/interfaces/user/authenticated-user.interface';
+import { verifyYourEmailHtml, verifyYourEmailPlainText } from '../pages/verify-your-email.page';
+import { ServerConfigService } from 'src/config/services/server.config.service';
+import { AuthTokenService } from '../types/auth-tokens-service.type';
+import { EmailsService } from 'src/emails/emails.service';
 
 @Injectable()
 export class AuthNotifications {
@@ -23,20 +19,20 @@ export class AuthNotifications {
 
     constructor(
         @Inject(ACCOUNT_VERIFICATION_TOKEN)
-        private readonly accVerifToken: TokensService<IAccVerifTokenPayload>,
+        private readonly accountVerificationToken: AuthTokenService,
         @Inject(ACCOUNT_DELETION_TOKEN)
-        private readonly accDeletionToken: TokensService<IAccDeletionTokenPayload>,
+        private readonly accountDeletionToken: AuthTokenService,
         private readonly emailsService: EmailsService,
         private readonly serverConfig: ServerConfigService,
     ) {}
 
     private async createAccountVerificationLink(id: string) {
-        const token = await this.accVerifToken.generate({ id });
+        const token = await this.accountVerificationToken.generate({ id });
         return `${this.serverConfig.apiBaseUrl}/auth/verify-account?token=${token}`;
     }
 
     private async createAccountDeletionLink(id: string) {
-        const token = await this.accDeletionToken.generate({ id });
+        const token = await this.accountDeletionToken.generate({ id });
         return `${this.serverConfig.apiBaseUrl}/auth/delete-account?token=${token}`;
     }
 
--- testing/suites/integration/setup/after-env/set-kit.ts
+++ testing/suites/integration/setup/after-env/set-kit.ts
@@ -1,19 +1,15 @@
-import { testKit } from '@integration/utils/test-kit.util';
-import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 import {
     ACCOUNT_DELETION_TOKEN,
     ACCOUNT_VERIFICATION_TOKEN,
 } from 'src/auth/constants/tokens.provider.constant';
-import {
-    IAccDeletionTokenPayload,
-    IAccVerifTokenPayload,
-} from 'src/auth/interfaces/tokens-payload.interface';
+import { testKit } from '@integration/utils/test-kit.util';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 import { RedisClientAdapter } from 'src/common/redis/redis.client.adapter';
 import { AuthConfigService } from 'src/config/services/auth.config.service';
+import { AuthTokenService } from 'src/auth/types/auth-tokens-service.type';
 import { UserSeedService } from 'src/seed/services/user-seed.service';
 import { SESSIONS_REDIS_CONNECTION } from 'src/sessions/constants/sessions.constants';
 import { TOKENS_REDIS_CONNECTION } from 'src/tokens/constants/tokens.constants';
-import { TokensService } from 'src/tokens/tokens.service';
 import { User } from 'src/users/entities/user.entity';
 import { DataSource } from 'typeorm';
 
@@ -23,10 +19,7 @@ beforeAll(() => {
     testKit.userRepos = testKit.app.get(DataSource).getRepository(User);
     testKit.tokensRedisClient = testKit.app.get<RedisClientAdapter>(TOKENS_REDIS_CONNECTION);
     testKit.sessionsRedisClient = testKit.app.get<RedisClientAdapter>(SESSIONS_REDIS_CONNECTION);
-    testKit.accDeletionToken =
-        testKit.app.get<TokensService<IAccDeletionTokenPayload>>(ACCOUNT_DELETION_TOKEN);
-    testKit.accVerifToken = testKit.app.get<TokensService<IAccVerifTokenPayload>>(
-        ACCOUNT_VERIFICATION_TOKEN,
-    );
+    testKit.accDeletionToken = testKit.app.get<AuthTokenService>(ACCOUNT_DELETION_TOKEN);
+    testKit.accVerifToken = testKit.app.get<AuthTokenService>(ACCOUNT_VERIFICATION_TOKEN);
     testKit.cacheManager = testKit.app.get<Cache>(CACHE_MANAGER);
 });
