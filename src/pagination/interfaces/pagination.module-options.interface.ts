201cb035e52969fe769131c9e5e9a7e988dc112d
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-14 17:45:18 -0500

    feat(pagination): make transformFunction generic in IPaginationModuleOptions

--- src/pagination/interfaces/pagination.module-options.interface.ts
+++ src/pagination/interfaces/pagination.module-options.interface.ts
@@ -1,7 +1,8 @@
 import { ObjectLiteral } from 'typeorm';
 
-export interface IPaginationModuleOptions {
+export interface IPaginationModuleOptions<T = ObjectLiteral> {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
     repositoryToken: Function | string;
-    transformFunction: (rawRecord: any) => ObjectLiteral;
+    transformFunction: (rawRecord: any) => T;
+    createCacheKeyFunction: (id: string) => string;
 }
