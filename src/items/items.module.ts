2d5d72026bba00a73cb4bcb7ffac5b779be69833
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-14 17:44:26 -0500

    feat(pagination): add createCacheKeyFunction to Items and Users modules

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -6,10 +6,12 @@ import { ItemsResolver } from './items.resolver';
 import { HttpLoggerModule } from 'src/http-logger/http-logger.module';
 import { PaginationModule } from 'src/pagination/pagination.module';
 import { rawRecordToItemEntity } from './functions/raw-record-to-item-entity';
+import { createItemCacheKey } from './cache/create-cache-key';
 
 @Module({
     imports: [
         PaginationModule.register({
+            createCacheKeyFunction: createItemCacheKey,
             repositoryToken: getRepositoryToken(Item),
             transformFunction: rawRecordToItemEntity,
         }),
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,12 +6,14 @@ import { Module } from '@nestjs/common';
 import { HttpLoggerModule } from 'src/http-logger/http-logger.module';
 import { PaginationModule } from 'src/pagination/pagination.module';
 import { rawRecordTouserEntity } from './functions/raw-record-to-user-entity';
+import { createUserCacheKey } from './cache/create-cache-key';
 
 @Module({
     imports: [
         PaginationModule.register({
-            transformFunction: rawRecordTouserEntity,
+            createCacheKeyFunction: createUserCacheKey,
             repositoryToken: getRepositoryToken(User),
+            transformFunction: rawRecordTouserEntity,
         }),
         HttpLoggerModule.forFeature({ context: UsersService.name }),
         TypeOrmModule.forFeature([User]),
