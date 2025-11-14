e9e3feb482be6e495c9b76295597e63f689310d3
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-14 17:45:32 -0500

    feat(pagination): refactor PaginationService to inject pagination cache service

--- src/pagination/pagination.service.ts
+++ src/pagination/pagination.service.ts
@@ -1,56 +1,38 @@
-import {
-    PAGINATION_CACHE_QUEUE,
-    REPOSITORY_TOKEN,
-    TRANSFORM_FUNCTION,
-} from './constants/pagination.constants';
-import { InjectQueue } from '@nestjs/bullmq';
-import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import { PaginatedRecord } from './interfaces/base-record.data';
-import { IDecodedCursor } from './interfaces/decoded-cursor.interface';
+import { IPaginationModuleOptions } from './interfaces/pagination.module-options.interface';
+import { PAGINATION_MODULE_OPTIONS_TOKEN } from './module/config.module-definition';
+import { PaginationCacheService } from './cache/pagination.cache.service';
 import { PaginationOptionsType } from './types/pagination.options.type';
 import { IPaginatedType } from './interfaces/paginated-type.interface';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { PaginatedRecord } from './interfaces/base-record.data';
 import { IEdgeType } from './interfaces/edge-type.interface';
-import { ObjectLiteral, Repository } from 'typeorm';
+import { BaseEntity } from 'src/common/entites/base.entity';
+import { encodeCursor } from './functions/encode-cursor';
+import { decodeCursor } from './functions/decode-cursor';
 import { ModuleRef } from '@nestjs/core';
-import { Queue } from 'bullmq';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PaginationService<T extends ObjectLiteral> implements OnModuleInit {
+export class PaginationService<T extends BaseEntity> implements OnModuleInit {
     private repository!: Repository<T>;
 
     constructor(
-        @Inject(TRANSFORM_FUNCTION)
-        private readonly transformFunction: (rawRecord: ObjectLiteral) => T,
-        @InjectQueue(PAGINATION_CACHE_QUEUE)
-        private readonly cacheQuue: Queue,
-        @Inject(REPOSITORY_TOKEN)
-        private readonly repositoryToken: string,
-        @Inject(CACHE_MANAGER)
-        private readonly cacheManager: Cache,
-        private moduleRef: ModuleRef,
+        @Inject(PAGINATION_MODULE_OPTIONS_TOKEN)
+        private readonly options: IPaginationModuleOptions<T>,
+        private readonly paginationCacheSvc: PaginationCacheService<T>,
+        private readonly moduleRef: ModuleRef,
     ) {}
 
     onModuleInit() {
-        this.repository = this.moduleRef.get<Repository<T>>(this.repositoryToken, {
+        this.repository = this.moduleRef.get<Repository<T>>(this.options.repositoryToken, {
             strict: false,
         });
     }
 
-    private decodeCursor(cursor: string): IDecodedCursor {
-        const json = Buffer.from(cursor, 'base64').toString('utf-8');
-        return JSON.parse(json) as IDecodedCursor;
-    }
-
-    private encodeCursor(createdAt: string, id: string): string {
-        const json = JSON.stringify({ createdAt: createdAt, id });
-        return Buffer.from(json).toString('base64');
-    }
-
     private createEdges(rawData: PaginatedRecord[]): IEdgeType<T>[] {
         const edges = rawData.map((rawRecord) => ({
-            cursor: this.encodeCursor(rawRecord.cursor, rawRecord.id),
-            node: this.transformFunction(rawRecord),
+            cursor: encodeCursor(rawRecord.cursor, rawRecord.id),
+            node: this.options.transformFunction(rawRecord),
         }));
         return edges;
     }
@@ -66,9 +48,8 @@ export class PaginationService<T extends ObjectLiteral> implements OnModuleInit 
         };
     }
 
-    // No cache version
     private async createPagination(limit: number, cursor: string): Promise<IPaginatedType<T>> {
-        const decodedCursor = cursor ? this.decodeCursor(cursor) : undefined;
+        const decodedCursor = cursor ? decodeCursor(cursor) : undefined;
         const rawData = await this.repository
             .createQueryBuilder()
             .select('*')
@@ -85,32 +66,9 @@ export class PaginationService<T extends ObjectLiteral> implements OnModuleInit 
         return await this.createPages(edges, limit);
     }
 
-    private async getPageIdsAndCursors(
-        limit: number,
-        decodedCursor?: IDecodedCursor,
-    ): Promise<PaginatedRecord[]> {
-        const pageIds = await this.repository
-            .createQueryBuilder()
-            .select('id')
-            .addSelect('created_at::text', 'cursor')
-            .where(decodedCursor ? '(created_at, id) > (:date, :id)' : 'TRUE', {
-                date: decodedCursor?.createdAt,
-                id: decodedCursor?.id,
-            })
-            .orderBy('created_at', 'ASC')
-            .addOrderBy('id', 'ASC')
-            .limit(limit + 1) // whether exists a next page or not
-            .getRawMany<{ id: string; cursor: string }>();
-        return pageIds.map(({ id, cursor }) => ({ id, cursor: this.encodeCursor(cursor, id) }));
-    }
-
-    // private async createPaginationCached(limit: number, cursor: string) {
-    //     const decodedCursor = cursor ? this.decodeCursor(cursor) : undefined;
-    //     const pageIds = await this.getPageIdsAndCursors(limit, decodedCursor);
-    //     console.log({ pageIds });
-    // }
-
     async create(options: PaginationOptionsType): Promise<IPaginatedType<T>> {
-        return await this.createPagination(options.limit, options.cursor);
+        return options.cache
+            ? await this.paginationCacheSvc.createPagination(options.limit, options.cursor)
+            : await this.createPagination(options.limit, options.cursor);
     }
 }
