d535c67ec5488f21b101955174135403b8804f5a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-19 14:21:46 -0500

    feat(items): introduce ITEMS_LIMITS constants for item validation

---
+++ src/items/constants/items.constants.ts
@@ -0,0 +1,20 @@
+export const ITEMS_LIMITS = {
+    TITLE: {
+        MIN: 5,
+        MAX: 40,
+    },
+    DESCRIPTION: {
+        MIN: 5,
+        MAX: 500,
+    },
+    CATEGORY: {
+        MIN: 3,
+        MAX: 40,
+    },
+    TAGS: {
+        MIN_ARRAY_SIZE: 1,
+        MAX_ARRAY_SIZE: 10,
+        TAG_MIN_LENGTH: 2,
+        TAG_MAX_LENGTH: 20,
+    },
+};
--- src/items/dtos/create-item.input.ts
+++ src/items/dtos/create-item.input.ts
@@ -10,35 +10,36 @@ import { Transform } from 'class-transformer';
 import { Field, InputType } from '@nestjs/graphql';
 import { trimAndLowercase } from 'src/common/functions/utils/trim-and-lowercase.util';
 import { trimAndLowerCaseArray } from 'src/common/functions/utils/trim-and-lowercase-array.util';
+import { ITEMS_LIMITS } from '../constants/items.constants';
 
 // TODO: use constants
 @InputType()
 export class CreateItemInput {
     @IsString()
-    @MinLength(5)
-    @MaxLength(20)
+    @MinLength(ITEMS_LIMITS.TITLE.MIN)
+    @MaxLength(ITEMS_LIMITS.TITLE.MAX)
     @Field(() => String)
     title!: string;
 
     @Field(() => String)
     @IsString()
-    @MinLength(5)
-    @MaxLength(200)
+    @MinLength(ITEMS_LIMITS.DESCRIPTION.MIN)
+    @MaxLength(ITEMS_LIMITS.DESCRIPTION.MAX)
     description!: string;
 
     @IsString()
-    @MinLength(5)
-    @MaxLength(20)
+    @MinLength(ITEMS_LIMITS.CATEGORY.MIN)
+    @MaxLength(ITEMS_LIMITS.CATEGORY.MAX)
     @Transform(trimAndLowercase)
     @Field(() => String)
     category!: string;
 
     @IsArray()
-    @ArrayMaxSize(5)
-    @ArrayMinSize(1)
+    @ArrayMaxSize(ITEMS_LIMITS.TAGS.MAX_ARRAY_SIZE)
+    @ArrayMinSize(ITEMS_LIMITS.TAGS.MIN_ARRAY_SIZE)
     @IsString({ each: true })
-    @MinLength(2, { each: true })
-    @MaxLength(20, { each: true })
+    @MinLength(ITEMS_LIMITS.TAGS.TAG_MIN_LENGTH, { each: true })
+    @MaxLength(ITEMS_LIMITS.TAGS.TAG_MAX_LENGTH, { each: true })
     @Transform(trimAndLowerCaseArray)
     @Field(() => [String])
     tags!: string[];
