e1cb7c458d0ed2c8fbc1f739694f246ad973b82b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-19 13:57:02 -0500

    feat(user): update UserModel to extend AccountModel instead of UserAccountModel

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,10 +1,10 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { UserAccountModel } from 'src/auth/models/user-account.model';
+import { AccountModel } from 'src/auth/models/account.model';
 import { Item } from 'src/items/entities/item.entity';
 import { ItemPaginationModel } from 'src/items/models/pagination.model';
 
 @ObjectType({ description: 'User model' })
-export class UserModel extends UserAccountModel {
+export class UserModel extends AccountModel {
     @Field(() => ItemPaginationModel)
     items!: Item[];
 }
