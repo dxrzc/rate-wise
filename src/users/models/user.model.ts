9aafa444cbffa1f9520a506c5784ae44f361259e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-19 13:54:51 -0500

    feat(user): extend UserModel from UserAccountModel to incorporate user account properties

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,22 +1,10 @@
-import { Field, Float, ObjectType } from '@nestjs/graphql';
-import { BaseModel } from 'src/common/models/base.model';
-import { AccountStatus } from '../enums/account-status.enum';
-import { UserRole } from '../enums/user-role.enum';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { UserAccountModel } from 'src/auth/models/user-account.model';
+import { Item } from 'src/items/entities/item.entity';
+import { ItemPaginationModel } from 'src/items/models/pagination.model';
 
 @ObjectType({ description: 'User model' })
-export class UserModel extends BaseModel {
-    @Field(() => String)
-    username!: string;
-
-    @Field(() => String)
-    email!: string;
-
-    @Field(() => [UserRole])
-    roles!: UserRole[];
-
-    @Field(() => AccountStatus)
-    status!: AccountStatus;
-
-    @Field(() => Float)
-    reputationScore!: number;
+export class UserModel extends UserAccountModel {
+    @Field(() => ItemPaginationModel)
+    items!: Item[];
 }
