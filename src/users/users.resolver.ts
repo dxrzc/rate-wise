e6768b0d098a25e2faf46cb137189a29c07154e3
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-19 20:18:13 -0500

    feat(users): update findOneById method to retrieve user and associated items and add todo

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -6,6 +6,7 @@ import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationArgs } from 'src/common/dtos/args/pagination.args';
 import { UserPaginationModel } from './models/pagination.model';
 import { IPaginatedType } from 'src/pagination/interfaces/paginated-type.interface';
+import { User } from './entities/user.entity';
 
 @Resolver(() => UserModel)
 export class UsersResolver {
@@ -15,14 +16,14 @@ export class UsersResolver {
     @RelaxedThrottle()
     @Query(() => UserModel, { name: 'findUserById' })
     async findOneById(@Args('user_id', { type: () => ID }) id: string): Promise<UserModel> {
-        return await this.userService.findOneByIdOrThrowCached(id);
+        return await this.userService.findUserAndItemsOrThrowCached(id);
     }
-    // TODO: find by email
 
+    // TODO: returns items too
     @Public()
     @BalancedThrottle()
     @Query(() => UserPaginationModel, { name: 'users' })
-    async findAll(@Args() paginationArgs: PaginationArgs): Promise<IPaginatedType<UserModel>> {
+    async findAll(@Args() paginationArgs: PaginationArgs): Promise<IPaginatedType<User>> {
         return await this.userService.findAll(paginationArgs);
     }
 }
