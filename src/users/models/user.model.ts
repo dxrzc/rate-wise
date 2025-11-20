20a1fd3e653589813a46ba0e481b62aac6ebb974
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-19 20:15:30 -0500

    feat(users): update items field to use IPaginatedType for improved pagination

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,10 +1,11 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { AccountModel } from 'src/auth/models/account.model';
-import { Item } from 'src/items/entities/item.entity';
+import { ItemModel } from 'src/items/models/item.model';
 import { ItemPaginationModel } from 'src/items/models/pagination.model';
+import { IPaginatedType } from 'src/pagination/interfaces/paginated-type.interface';
 
 @ObjectType({ description: 'User model' })
 export class UserModel extends AccountModel {
     @Field(() => ItemPaginationModel)
-    items!: Item[];
+    items!: IPaginatedType<ItemModel>;
 }
