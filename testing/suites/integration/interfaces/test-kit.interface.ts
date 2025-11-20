2876ae0129eb276de124703a497160cf26a1a19d
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-20 17:13:10 -0500

    refactor(test-kit): replace TokensService with AuthTokenService for account tokens

--- testing/suites/integration/interfaces/test-kit.interface.ts
+++ testing/suites/integration/interfaces/test-kit.interface.ts
@@ -7,12 +7,8 @@ import { AuthConfigService } from 'src/config/services/auth.config.service';
 import { Test } from 'supertest';
 import { RedisClientAdapter } from 'src/common/redis/redis.client.adapter';
 import { RestClient } from './rest-client.interface';
-import { TokensService } from 'src/tokens/tokens.service';
-import {
-    IAccDeletionTokenPayload,
-    IAccVerifTokenPayload,
-} from 'src/auth/interfaces/tokens-payload.interface';
 import { Cache } from '@nestjs/cache-manager';
+import { AuthTokenService } from 'src/auth/types/auth-tokens-service.type';
 
 export interface ITestKit {
     app: INestApplication<App>;
@@ -24,8 +20,8 @@ export interface ITestKit {
     cacheManager: Cache;
     gqlClient: Test;
     restClient: RestClient;
-    accDeletionToken: TokensService<IAccDeletionTokenPayload>;
-    accVerifToken: TokensService<IAccVerifTokenPayload>;
+    accDeletionToken: AuthTokenService;
+    accVerifToken: AuthTokenService;
     endpointsREST: {
         verifyAccount: string;
         deleteAccount: string;
