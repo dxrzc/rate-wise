7446918a67588c8c5d6b2ce02f252d097ee191b8
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-24 11:50:54 -0500

    feat(seed): add createReviews method to SeedService for seeding review data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -9,6 +9,8 @@ import { getRandomUserRoles } from './functions/get-random-user-roles';
 import { getRandomAccountStatus } from './functions/get-random-account-status';
 import { Item } from 'src/items/entities/item.entity';
 import { ItemsSeedService } from './services/items-seed.service';
+import { Review } from 'src/reviews/entities/review.entity';
+import { ReviewSeedService } from './services/reviews-seed.service';
 
 @Injectable()
 export class SeedService {
@@ -17,8 +19,11 @@ export class SeedService {
         private readonly userRepository: Repository<User>,
         @InjectRepository(Item)
         private readonly itemRepository: Repository<Item>,
+        @InjectRepository(Review)
+        private readonly reviewRepository: Repository<Review>,
         private readonly usersSeed: UserSeedService,
         private readonly itemsSeed: ItemsSeedService,
+        private readonly reviewsSeed: ReviewSeedService,
         private readonly logger: HttpLoggerService,
     ) {}
 
@@ -40,6 +45,7 @@ export class SeedService {
         this.logger.debug(`${n} users seeded`);
     }
 
+    // item per user
     async createItems(n: number): Promise<void> {
         // delete existing items first
         await this.itemRepository.deleteAll();
@@ -50,6 +56,35 @@ export class SeedService {
         for (const id of usersIds) {
             for (let i = 0; i < n; i++)
                 promises.push(this.itemRepository.save({ ...this.itemsSeed.item, user: { id } }));
+        }
+        await Promise.all(promises);
+    }
+
+    // reviews per item, user id chosen randomly
+    async createReviews(n: number): Promise<void> {
+        // delete existing reviews first
+        await this.reviewRepository.deleteAll();
+        // fetch users
+        const usersInDb = await this.userRepository.find({ select: { id: true } });
+        if (usersInDb.length === 0) throw new Error('No users found. Seed users first');
+        const usersIds = usersInDb.map((e) => e.id);
+        // fetch items
+        const itemsInDb = await this.itemRepository.find({ select: { id: true } });
+        if (itemsInDb.length === 0) throw new Error('No items found. Seed users first');
+        const itemsIds = itemsInDb.map((e) => e.id);
+        // seed
+        const promises = new Array<Promise<unknown>>();
+        for (const itemId of itemsIds) {
+            for (let i = 0; i < n; i++) {
+                const randomUserId = usersIds[Math.floor(Math.random() * usersIds.length)];
+                promises.push(
+                    this.reviewRepository.save({
+                        ...this.reviewsSeed.review,
+                        relatedItem: itemId,
+                        createdBy: randomUserId,
+                    }),
+                );
+            }
         }
         await Promise.all(promises);
     }
