dc6b6c00c68e3a81d94cfb1e547a75b5c229f5f2
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-24 18:26:59 -0500

    fix(createItem): correct item data reference in createItem function

--- testing/suites/integration/utils/create-item.util.ts
+++ testing/suites/integration/utils/create-item.util.ts
@@ -1,8 +1,11 @@
 import { testKit } from './test-kit.util';
 import { Item } from 'src/items/entities/item.entity';
 
+/**
+ * Creates an item in database with default values
+ */
 export async function createItem(userId: string): Promise<Item> {
-    const itemData = testKit.itemSeed.item;
+    const itemData = testKit.itemSeed.itemInput;
     const created = await testKit.itemRepos.save({ ...itemData, createdBy: userId });
     return created;
 }
