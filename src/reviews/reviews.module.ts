722696e720b7bbb8d2d8430157ead0f30b2d86ec
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-24 18:26:34 -0500

    feat(reviews): add imports for TypeOrmModule and HttpLoggerModule

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ReviewService as ReviewsService } from './reviews.service';
 import { ReviewResolver as ReviewsResolver } from './reviews.resolver';
+import { Review } from './entities/review.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemsModule } from 'src/items/items.module';
+import { Item } from 'src/items/entities/item.entity';
+import { HttpLoggerModule } from 'src/http-logger/http-logger.module';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([Review, Item]),
+        ItemsModule,
+        HttpLoggerModule.forFeature({ context: 'Reviews' }),
+    ],
     providers: [ReviewsService, ReviewsResolver],
 })
 export class ReviewsModule {}
