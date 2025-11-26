fa1181bc6c64b0b91df68e10ab3296df272e1e74
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-26 14:37:21 -0500

    fix(create-review): correct review seed reference in createReview function

--- testing/suites/integration/utils/create-review.util.ts
+++ testing/suites/integration/utils/create-review.util.ts
@@ -1,9 +1,12 @@
 import { Review } from 'src/reviews/entities/review.entity';
 import { testKit } from './test-kit.util';
 
+/**
+ * Creates a review with default values
+ */
 export async function createReview(itemId: string, userId: string): Promise<Review> {
     return await testKit.reviewRepos.save({
-        ...testKit.reviewSeed.review,
+        ...testKit.reviewSeed.reviewInput,
         relatedItem: itemId,
         createdBy: userId,
     });
