eddfdbacd7db557b7de2f8ef9af2b9426c790b19
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-02 19:04:08 -0500

    fix(migration): remove CONCURRENTLY from index creation for better typeorm compatibility

--- db/migrations/1764716490373-AddPaginationIndexes.ts
+++ db/migrations/1764716490373-AddPaginationIndexes.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddPaginationIndexes1764716490373 implements MigrationInterface {
-    public static readonly transaction = false;
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE INDEX CONCURRENTLY account_idx_created_id ON account (created_at, id)`);
-        await queryRunner.query(`CREATE INDEX CONCURRENTLY item_idx_created_id ON item (created_at, id)`);
-        await queryRunner.query(`CREATE INDEX CONCURRENTLY review_idx_created_id ON review (created_at, id)`);
+        await queryRunner.query(`CREATE INDEX account_idx_created_id ON account (created_at, id)`);
+        await queryRunner.query(`CREATE INDEX item_idx_created_id ON item (created_at, id)`);
+        await queryRunner.query(`CREATE INDEX review_idx_created_id ON review (created_at, id)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
