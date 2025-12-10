01369ef48d7b9ca02b4ec6c957119e118a899db6
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-09 19:00:31 -0500

    feat(votes): add CreateVoteInput DTO for vote creation

---
+++ src/votes/dtos/create-vote.input.ts
@@ -0,0 +1,23 @@
+import { InputType } from '@nestjs/graphql';
+import { Field } from '@nestjs/graphql';
+import { IsDefined, IsString, IsUUID } from 'class-validator';
+import { VoteAction } from '../enum/vote.enum';
+
+@InputType({
+    description: 'Input data required to create a new vote for a review.',
+})
+export class CreateVoteInput {
+    @IsDefined()
+    @Field(() => VoteAction, {
+        description: 'The action of the vote, either UP or DOWN.',
+    })
+    vote!: VoteAction;
+
+    @IsDefined()
+    @IsUUID()
+    @IsString()
+    @Field(() => String, {
+        description: 'The unique ID of the review being voted on.',
+    })
+    reviewId!: string;
+}
