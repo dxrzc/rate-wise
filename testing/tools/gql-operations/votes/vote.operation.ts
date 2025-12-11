f87c92119b256a9891042b42466b558dc60d5224
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-10 21:51:26 -0500

    feat(votes): update voteReview function to use inline argument types

--- testing/tools/gql-operations/votes/vote.operation.ts
+++ testing/tools/gql-operations/votes/vote.operation.ts
@@ -1,8 +1,7 @@
-import { CreateVoteInput } from 'src/votes/dtos/create-vote.input';
 import { operationFactory } from '../factory/operation.factory';
 import { IOperation } from '../interfaces/operation.interface';
 
-export function voteReview({ args, fields }: IOperation<CreateVoteInput, void>) {
+export function voteReview({ args, fields }: IOperation<{ vote: string; reviewId: string }, void>) {
     return operationFactory(
         {
             operationName: 'voteReview',
