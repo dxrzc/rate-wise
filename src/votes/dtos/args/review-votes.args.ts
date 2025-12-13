89f310b650935f96123e289d6f0500fb576d5f81
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-13 13:45:40 -0500

    feat(votes): update ReviewVotesArgs to enforce reviewId as a defined ID type

--- src/votes/dtos/args/review-votes.args.ts
+++ src/votes/dtos/args/review-votes.args.ts
@@ -1,9 +1,11 @@
-import { ArgsType, Field } from '@nestjs/graphql';
+import { ArgsType, Field, ID } from '@nestjs/graphql';
+import { IsDefined } from 'class-validator';
 import { PaginationArgs } from 'src/common/dtos/args/pagination.args';
 
 @ArgsType()
 export class ReviewVotesArgs extends PaginationArgs {
-    @Field(() => String, {
+    @IsDefined()
+    @Field(() => ID, {
         description: 'The unique ID of the review for which to fetch votes.',
     })
     reviewId!: string;
