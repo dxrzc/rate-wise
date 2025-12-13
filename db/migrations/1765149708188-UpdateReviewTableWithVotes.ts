e40ee3030c0a0f8d5624ef650837cb062cf31b14
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-13 14:22:54 -0500

    feat(migrations): add missing awaits in last migration file

--- db/migrations/1765149708188-UpdateReviewTableWithVotes.ts
+++ db/migrations/1765149708188-UpdateReviewTableWithVotes.ts
@@ -3,16 +3,16 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class UpdateReviewTableWithVotes1765149708188 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         // delete old "votes" column
-        queryRunner.query(`
+        await queryRunner.query(`
             ALTER TABLE "review" DROP COLUMN "votes";
         `);
 
         // add "upvotes" and "downvote column"
-        queryRunner.query(`
+        await queryRunner.query(`
             ALTER TABLE "review"
             ADD COLUMN "upvotes" INTEGER NOT NULL DEFAULT 0;
         `);
-        queryRunner.query(`
+        await queryRunner.query(`
             ALTER TABLE "review"
             ADD COLUMN "downvotes" INTEGER NOT NULL DEFAULT 0;
         `);
@@ -20,15 +20,15 @@ export class UpdateReviewTableWithVotes1765149708188 implements MigrationInterfa
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         // remove "upvotes" and "downvotes" columns
-        queryRunner.query(`
+        await queryRunner.query(`
             ALTER TABLE "review" DROP COLUMN "upvotes";
         `);    
-        queryRunner.query(`
+        await queryRunner.query(`
             ALTER TABLE "review" DROP COLUMN "downvotes";
         `);
 
         // add old "votes" column
-        queryRunner.query(`
+        await queryRunner.query(`
             ALTER TABLE "review"
             ADD COLUMN "votes" INTEGER NOT NULL DEFAULT 0;
         `);
