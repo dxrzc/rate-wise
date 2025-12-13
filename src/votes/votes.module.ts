ed1bd56a26c929fb62a2615c48f1e29860d07e76
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-13 13:06:07 -0500

    feat(votes): register PaginationModule with cache key and repository token for votes

--- src/votes/votes.module.ts
+++ src/votes/votes.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { VotesResolver } from './votes.resolver';
 import { VotesService } from './votes.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { Vote } from './entities/vote.entity';
 import { HttpLoggerModule } from 'src/http-logger/http-logger.module';
 import { ReviewsModule } from 'src/reviews/reviews.module';
+import { PaginationModule } from 'src/pagination/pagination.module';
+import { createVoteCacheKey } from './cache/create-cache-key';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Vote]),
         HttpLoggerModule.forFeature({ context: 'votes' }),
         ReviewsModule,
+        PaginationModule.register({
+            createCacheKeyFunction: createVoteCacheKey,
+            repositoryToken: getRepositoryToken(Vote),
+        }),
     ],
     providers: [VotesResolver, VotesService],
 })
