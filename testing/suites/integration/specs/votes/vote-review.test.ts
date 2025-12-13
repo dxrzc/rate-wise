d5c79b1b27a124ddbf87d0fa37a2d83f9dd5fca7
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-13 12:29:21 -0500

    feat(votes): enhance voteReview tests with additional scenarios and input validation

--- testing/suites/integration/specs/votes/vote-review.test.ts
+++ testing/suites/integration/specs/votes/vote-review.test.ts
@@ -1,4 +1,6 @@
 import { createAccount } from '@integration/utils/create-account.util';
+import { createReview } from '@integration/utils/create-review.util';
+import { success } from '@integration/utils/no-errors.util';
 import { testKit } from '@integration/utils/test-kit.util';
 import { voteReview } from '@testing/tools/gql-operations/votes/vote.operation';
 import { AUTH_MESSAGES } from 'src/auth/messages/auth.messages';
@@ -9,53 +11,68 @@ import { AccountStatus } from 'src/users/enums/account-status.enum';
 import { UserRole } from 'src/users/enums/user-role.enum';
 import { VoteAction } from 'src/votes/enum/vote.enum';
 
-describe('Gql - upVoteReview', () => {
+// GraphQL expects the keys not the values. Keys are in uppercase.
+const inputVotes = {
+    UP: VoteAction.UP.toUpperCase(),
+    DOWN: VoteAction.DOWN.toUpperCase(),
+};
+
+describe('Gql - voteReview', () => {
     describe('Session cookie not provided', () => {
         test('return unauthorized code and unauthorized error message', async () => {
             const response = await testKit.gqlClient.send(
-                voteReview({ args: { reviewId: 'some-review-id', vote: VoteAction.UP } }),
+                voteReview({ args: { reviewId: 'some-review-id', vote: inputVotes.UP } }),
             );
-            console.log(response.body);
             expect(response).toFailWith(Code.UNAUTHORIZED, AUTH_MESSAGES.UNAUTHORIZED);
         });
     });
 
     describe.each(['reviewId', 'vote'] as const)('Missing required field: %s', (missingField) => {
-        test('return bad request code and invalid input error message', async () => {
+        test('return bad user input code', async () => {
             const { sessionCookie } = await createAccount({
                 roles: [UserRole.REVIEWER],
                 status: AccountStatus.ACTIVE,
             });
             const voteData: Record<string, any> = {
                 reviewId: 'some-review-id',
-                vote: VoteAction.UP,
+                vote: inputVotes.UP,
             };
             delete voteData[missingField];
             const response = await testKit.gqlClient
                 .send(voteReview({ args: voteData as any }))
                 .set('Cookie', sessionCookie);
-            expect(response).toFailWith(Code.BAD_REQUEST, COMMON_MESSAGES.INVALID_INPUT);
+            expect(response).toFailWith(Code.BAD_USER_INPUT, expect.stringContaining(missingField));
         });
     });
 
-    describe.each([AccountStatus.PENDING_VERIFICATION, AccountStatus.SUSPENDED])(
-        'Account status is %s',
-        (status: AccountStatus) => {
-            test('user can not perform this action (forbidden code and error message)', async () => {
-                const { sessionCookie } = await createAccount({
-                    roles: [UserRole.REVIEWER],
-                    status,
-                });
-                const response = await testKit.gqlClient
-                    .send(voteReview({ args: { reviewId: 'some-review-id', vote: VoteAction.UP } }))
-                    .set('Cookie', sessionCookie);
-                expect(response).toFailWith(Code.FORBIDDEN, AUTH_MESSAGES.FORBIDDEN);
+    describe('Account status is pending verification', () => {
+        test('return forbidden code and account is not active error message', async () => {
+            const { sessionCookie } = await createAccount({
+                roles: [UserRole.REVIEWER],
+                status: AccountStatus.PENDING_VERIFICATION,
             });
-        },
-    );
+            const response = await testKit.gqlClient
+                .send(voteReview({ args: { reviewId: 'some-review-id', vote: inputVotes.UP } }))
+                .set('Cookie', sessionCookie);
+            expect(response).toFailWith(Code.FORBIDDEN, AUTH_MESSAGES.ACCOUNT_IS_NOT_ACTIVE);
+        });
+    });
+
+    describe('Account status is suspended', () => {
+        test('return forbidden code and account is suspended error message', async () => {
+            const { sessionCookie } = await createAccount({
+                roles: [UserRole.REVIEWER],
+                status: AccountStatus.SUSPENDED,
+            });
+            const response = await testKit.gqlClient
+                .send(voteReview({ args: { reviewId: 'some-review-id', vote: inputVotes.UP } }))
+                .set('Cookie', sessionCookie);
+            expect(response).toFailWith(Code.FORBIDDEN, AUTH_MESSAGES.ACCOUNT_IS_SUSPENDED);
+        });
+    });
 
     describe.each([UserRole.MODERATOR, UserRole.ADMIN, UserRole.CREATOR])(
-        'Account roles are [%s]',
+        'Account is active and roles are [%s]',
         (role: UserRole) => {
             test('user can not perform this action (forbidden code and error message)', async () => {
                 const { sessionCookie } = await createAccount({
@@ -63,7 +80,7 @@ describe('Gql - upVoteReview', () => {
                     status: AccountStatus.ACTIVE,
                 });
                 const response = await testKit.gqlClient
-                    .send(voteReview({ args: { reviewId: 'some-review-id', vote: VoteAction.UP } }))
+                    .send(voteReview({ args: { reviewId: 'some-review-id', vote: inputVotes.UP } }))
                     .set('Cookie', sessionCookie);
                 expect(response).toFailWith(Code.FORBIDDEN, AUTH_MESSAGES.FORBIDDEN);
             });
@@ -76,12 +93,200 @@ describe('Gql - upVoteReview', () => {
                 roles: [UserRole.REVIEWER],
                 status: AccountStatus.ACTIVE,
             });
+            const { id: reviewId } = await createReview();
+            // delete review
+            await testKit.reviewRepos.delete(reviewId);
+            // try to vote the deleted review
             const response = await testKit.gqlClient
-                .send(voteReview({ args: { reviewId: 'some-review-id', vote: VoteAction.UP } }))
+                .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.UP } }))
                 .set('Cookie', sessionCookie);
             expect(response).toFailWith(Code.NOT_FOUND, REVIEW_MESSAGES.NOT_FOUND);
         });
     });
 
-    describe('Review id is not a valid uuid', () => {});
+    describe('Review id is not a valid uuid', () => {
+        test('return bad request code and invalid input error message', async () => {
+            const { sessionCookie } = await createAccount({
+                roles: [UserRole.REVIEWER],
+                status: AccountStatus.ACTIVE,
+            });
+            const response = await testKit.gqlClient
+                .send(voteReview({ args: { reviewId: 'invalid-uuid', vote: inputVotes.UP } }))
+                .set('Cookie', sessionCookie);
+            expect(response).toFailWith(Code.BAD_REQUEST, COMMON_MESSAGES.INVALID_INPUT);
+        });
+    });
+
+    describe('User has downvoted the review', () => {
+        describe('User upvotes the review', () => {
+            test('remove downvote and add upvote', async () => {
+                const { id: reviewId } = await createReview();
+                const { sessionCookie } = await createAccount({
+                    roles: [UserRole.REVIEWER],
+                    status: AccountStatus.ACTIVE,
+                });
+                // downvote the review
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.DOWN } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // upvote the same review
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.UP } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // should exist 1 vote with UP action
+                const votes = await testKit.votesRepos.find({
+                    where: { review: { id: reviewId } },
+                });
+                expect(votes).toHaveLength(1);
+                expect(votes[0].vote).toBe(VoteAction.UP);
+            });
+        });
+
+        describe('User downvotes the review again', () => {
+            test('do nothing and return success', async () => {
+                const { id: reviewId } = await createReview();
+                const { sessionCookie } = await createAccount({
+                    roles: [UserRole.REVIEWER],
+                    status: AccountStatus.ACTIVE,
+                });
+                // downvote the review
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.DOWN } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // downvote the same review again
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.DOWN } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // should exist 1 vote with DOWN action
+                const votes = await testKit.votesRepos.find({
+                    where: { review: { id: reviewId } },
+                });
+                expect(votes).toHaveLength(1);
+                expect(votes[0].vote).toBe(VoteAction.DOWN);
+            });
+        });
+    });
+
+    describe('User has upvoted the review', () => {
+        describe('User downvotes the review', () => {
+            test('remove upvote and add downvote', async () => {
+                const { id: reviewId } = await createReview();
+                const { sessionCookie } = await createAccount({
+                    roles: [UserRole.REVIEWER],
+                    status: AccountStatus.ACTIVE,
+                });
+                // upvote the review
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.UP } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // downvote the same review
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.DOWN } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // should exist 1 vote with DOWN action
+                const votes = await testKit.votesRepos.find({
+                    where: { review: { id: reviewId } },
+                });
+                expect(votes).toHaveLength(1);
+                expect(votes[0].vote).toBe(VoteAction.DOWN);
+            });
+        });
+
+        describe('User upvotes the review again', () => {
+            test('do nothing and return success', async () => {
+                const { id: reviewId } = await createReview();
+                const { sessionCookie } = await createAccount({
+                    roles: [UserRole.REVIEWER],
+                    status: AccountStatus.ACTIVE,
+                });
+                // upvote the review
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.UP } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // upvote the same review again
+                await testKit.gqlClient
+                    .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.UP } }))
+                    .set('Cookie', sessionCookie)
+                    .expect(success);
+                // should exist 1 vote with UP action
+                const votes = await testKit.votesRepos.find({
+                    where: { review: { id: reviewId } },
+                });
+                expect(votes).toHaveLength(1);
+                expect(votes[0].vote).toBe(VoteAction.UP);
+            });
+        });
+    });
+
+    describe('User is deleted', () => {
+        test('remove vote from review', async () => {
+            const { id: reviewId } = await createReview();
+            const { sessionCookie, id: userId } = await createAccount({
+                roles: [UserRole.REVIEWER],
+                status: AccountStatus.ACTIVE,
+            });
+            // upvote the review
+            await testKit.gqlClient
+                .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.UP } }))
+                .set('Cookie', sessionCookie)
+                .expect(success);
+            // verify vote exists
+            const votesBefore = await testKit.votesRepos.find({
+                where: { review: { id: reviewId } },
+            });
+            expect(votesBefore).toHaveLength(1);
+            // delete user
+            await testKit.userRepos.delete(userId);
+            // verify vote is removed
+            const votesAfter = await testKit.votesRepos.find({
+                where: { review: { id: reviewId } },
+            });
+            expect(votesAfter).toHaveLength(0);
+        });
+    });
+
+    describe('Vote successfully created', () => {
+        test('update review downvotes if downvoted', async () => {
+            const { id: reviewId } = await createReview();
+            const { sessionCookie } = await createAccount({
+                roles: [UserRole.REVIEWER],
+                status: AccountStatus.ACTIVE,
+            });
+            // downvote the review
+            await testKit.gqlClient
+                .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.DOWN } }))
+                .set('Cookie', sessionCookie)
+                .expect(success);
+            // fetch the review and verify downvotes count
+            const review = await testKit.reviewRepos.findOne({
+                where: { id: reviewId },
+            });
+            expect(review?.downVotes).toBe(1);
+        });
+
+        test('update review upvotes if upvoted', async () => {
+            const { id: reviewId } = await createReview();
+            const { sessionCookie } = await createAccount({
+                roles: [UserRole.REVIEWER],
+                status: AccountStatus.ACTIVE,
+            });
+            // upvote the review
+            await testKit.gqlClient
+                .send(voteReview({ args: { reviewId: reviewId, vote: inputVotes.UP } }))
+                .set('Cookie', sessionCookie)
+                .expect(success);
+            // fetch the review and verify upvotes count
+            const review = await testKit.reviewRepos.findOne({
+                where: { id: reviewId },
+            });
+            expect(review?.upVotes).toBe(1);
+        });
+    });
 });
