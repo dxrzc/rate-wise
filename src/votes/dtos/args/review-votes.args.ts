faca0513f51db363cd8e5db5bdb248ab10fdd21c
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-13 13:06:18 -0500

    feat(votes): extend ReviewVotesArgs to inherit pagination functionality

--- src/votes/dtos/args/review-votes.args.ts
+++ src/votes/dtos/args/review-votes.args.ts
@@ -1,7 +1,8 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { PaginationArgs } from 'src/common/dtos/args/pagination.args';
 
 @ArgsType()
-export class ReviewVotesArgs {
+export class ReviewVotesArgs extends PaginationArgs {
     @Field(() => String, {
         description: 'The unique ID of the review for which to fetch votes.',
     })
