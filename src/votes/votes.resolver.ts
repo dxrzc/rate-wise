2590ea45995e17c1d5685d4754719c6a5c559f7a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-13 13:45:53 -0500

    feat(votes): add findAllVotesForReview query with throttling and public access

--- src/votes/votes.resolver.ts
+++ src/votes/votes.resolver.ts
@@ -1,6 +1,6 @@
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { VotesService } from './votes.service';
-import { RelaxedThrottle } from 'src/common/decorators/throttling.decorator';
+import { BalancedThrottle, RelaxedThrottle } from 'src/common/decorators/throttling.decorator';
 import { MinAccountStatusRequired } from 'src/common/decorators/min-account-status.decorator';
 import { AccountStatus } from 'src/users/enums/account-status.enum';
 import { Roles } from 'src/common/decorators/roles.decorator';
@@ -8,6 +8,10 @@ import { UserRole } from 'src/users/enums/user-role.enum';
 import { voteReview } from './docs/voteReview.docs';
 import { RequestContext } from 'src/auth/types/request-context.type';
 import { CreateVoteInput } from './dtos/create-vote.input';
+import { ReviewVotesArgs } from './dtos/args/review-votes.args';
+import { Public } from 'src/common/decorators/public.decorator';
+import { findAllReviewVotesDocs } from './docs/findAllReviewVotes.docs';
+import { VotePaginationModel } from './models/pagination.model';
 
 @Resolver()
 export class VotesResolver {
@@ -23,5 +27,12 @@ export class VotesResolver {
     ) {
         await this.votesService.voteReview(voteData.reviewId, req.user, voteData.vote);
         return true;
+    }
+
+    @Public()
+    @BalancedThrottle()
+    @Query(() => VotePaginationModel, findAllReviewVotesDocs)
+    async findAllVotesForReview(@Args() args: ReviewVotesArgs) {
+        return await this.votesService.findAllVotesForReview(args);
     }
 }
