42d931174530bd9200f60e8c832f56a1bbc25900
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-13 12:43:50 -0500

    feat(votes): add VoteModel to represent user votes for reviews

---
+++ src/votes/models/vote.model.ts
@@ -0,0 +1,23 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { VoteAction } from '../enum/vote.enum';
+import { BaseModel } from 'src/common/models/base.model';
+
+@ObjectType({
+    description: `
+        Vote model representing a user's vote for a review.
+    `,
+})
+export class VoteModel extends BaseModel {
+    @Field(() => VoteAction, { description: `` })
+    vote!: VoteAction;
+
+    @Field(() => String, {
+        description: 'The ID of the user who created this vote.',
+    })
+    createdBy!: string;
+
+    @Field(() => String, {
+        description: 'The ID of the review this vote is for.',
+    })
+    relatedReview!: string;
+}
