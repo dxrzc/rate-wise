b70bbdc5e00d82d5cd4f13e772cffc16bb239d48
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-26 13:36:18 -0500

    feat(errors): add utility function to check for unique violation errors

---
+++ src/common/functions/error/is-unique-violation-error.ts
@@ -0,0 +1,7 @@
+import { QueryFailedError } from 'typeorm';
+
+export const isUniqueViolationError = (error: unknown): boolean => {
+    if (!(error instanceof QueryFailedError)) return false;
+    const driverError = error.driverError as { code?: string };
+    return driverError?.code === '23505';
+};
