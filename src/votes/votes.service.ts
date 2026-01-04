a06c07f7bd1662d92e2ad4668f8810076e012c60
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-01-03 20:48:24 -0500

    fix(votes): remove unsafe transaction when substracting user votes from reviews

--- src/votes/votes.service.ts
+++ src/votes/votes.service.ts
@@ -102,16 +102,12 @@ export class VotesService {
     }
 
     async subtractUserVotesFromReviews(userId: string, manager: EntityManager): Promise<void> {
-        const [upVotes, downVotes] = await Promise.all([
-            manager.withRepository(this.voteRepository).find({
-                where: { createdBy: userId, vote: VoteAction.UP },
-                select: ['relatedReview', 'vote'],
-            }),
-            manager.withRepository(this.voteRepository).find({
-                where: { createdBy: userId, vote: VoteAction.DOWN },
-                select: ['relatedReview', 'vote'],
-            }),
-        ]);
+        const votes = await manager.withRepository(this.voteRepository).find({
+            where: { createdBy: userId },
+            select: ['relatedReview', 'vote'],
+        });
+        const upVotes = votes.filter((v) => v.vote === VoteAction.UP);
+        const downVotes = votes.filter((v) => v.vote === VoteAction.DOWN);
         await this.reviewService.deleteVoteInMultipleReviews(
             upVotes.map((v) => v.relatedReview),
             VoteAction.UP,
