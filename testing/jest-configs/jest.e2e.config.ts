ed881f84cef88ab5e77cb24176aa2d7e66fac301
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-01-05 15:21:11 -0500

    feat(tests): add Jest configuration for end-to-end testing

---
+++ testing/jest-configs/jest.e2e.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+import { baseJestConfig } from './jest.base.config';
+
+const config: Config = {
+    ...baseJestConfig,
+    testTimeout: 60000,
+    maxWorkers: 2,
+
+    roots: ['<rootDir>/testing/suites/e2e/specs'],
+    setupFilesAfterEnv: ['<rootDir>/testing/suites/e2e/setup/after-env/set-kit.ts'],
+    moduleNameMapper: {
+        ...baseJestConfig.moduleNameMapper,
+        '^@e2e/(.*)$': '<rootDir>/testing/suites/e2e/$1',
+    },
+};
+
+export default config;
