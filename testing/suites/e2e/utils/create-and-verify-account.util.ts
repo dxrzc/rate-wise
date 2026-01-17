b55d29d1a9c197946a67a6a01bd785b19640c7ae
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-01-17 16:34:38 -0500

    feat(e2e): add util for creating and verifying account

---
+++ testing/suites/e2e/utils/create-and-verify-account.util.ts
@@ -0,0 +1,21 @@
+import { signUp } from '@testing/tools/gql-operations/auth/sign-up.operation';
+import { e2eKit } from './e2e-kit.util';
+import { requestAccountVerification } from '@testing/tools/gql-operations/auth/request-account-verification.operation';
+import { EmailPaths } from '@e2e/enum/email-paths.enum';
+import { HttpClient } from '@e2e/client/http.client';
+
+type AccountCreated = {
+    client: HttpClient;
+};
+
+export async function createAndVerifyAccount(): Promise<AccountCreated> {
+    const client = e2eKit.httpClient.getNewClient();
+    const user = e2eKit.usersSeed.signUpInput;
+    const userCreation = await client.graphQL(signUp({ args: user, fields: ['id'] }));
+    expect(userCreation).notToFail();
+    const reqAccVerif = await client.graphQL(requestAccountVerification());
+    expect(reqAccVerif).notToFail();
+    const link = await e2eKit.emailClient.getLinkSent(EmailPaths.verifyAccount, user.email);
+    await client.get(link!);
+    return { client };
+}
