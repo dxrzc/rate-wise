a7bcc4d473fb1bece7a32325823ddbecafb54c4b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-01-22 16:56:27 -0500

    refactor(tests): create common **extractSessionIdFromCookie** function for tests

--- testing/suites/integration/utils/get-sid-from-cookie.util.ts
+++ testing/suites/integration/utils/get-sid-from-cookie.util.ts
@@ -1,6 +1,5 @@
+import { extractSessionIdFromCookie } from '@testing/tools/utils/get-sid-from-cookie.util';
+
 export function getSidFromCookie(rawSessionCookie: string): string {
-    const undecodedSession = rawSessionCookie.split(';')[0].split('=')[1];
-    const decoded = decodeURIComponent(undecodedSession);
-    const sid = decoded.slice(2).split('.')[0];
-    return sid;
+    return extractSessionIdFromCookie(rawSessionCookie);
 }
