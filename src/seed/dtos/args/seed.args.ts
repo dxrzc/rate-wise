3b8a0be59c005ed01e8d1466a36578b9bb43933f
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-01-21 20:04:13 -0500

    feat(seed): change sed input for seed args

--- src/seed/dtos/args/seed.args.ts
+++ src/seed/dtos/args/seed.args.ts
@@ -1,23 +1,23 @@
-import { Field, InputType, Int } from '@nestjs/graphql';
-import { IsDefined, IsInt, IsPositive } from 'class-validator';
+import { ArgsType, Field, Int } from '@nestjs/graphql';
+import { IsInt, IsPositive } from 'class-validator';
 
-@InputType({
-    description: 'Input data required for seeding the database.',
-})
-export class SeedInput {
-    @IsDefined()
+@ArgsType()
+export class SeedArgs {
     @IsInt()
     @IsPositive()
     @Field(() => Int, {
         description: 'Number of users to create during the seeding process.',
+        defaultValue: 10,
+        nullable: true,
     })
     users!: number;
 
-    @IsDefined()
     @IsInt()
     @IsPositive()
     @Field(() => Int, {
         description: 'Number of items to create per user during the seeding process.',
+        defaultValue: 3,
+        nullable: true,
     })
     itemsPerUser!: number;
 }
--- src/seed/seed.resolver.ts
+++ src/seed/seed.resolver.ts
@@ -2,8 +2,8 @@ import { Controller } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { Args, Mutation } from '@nestjs/graphql';
 import { Public } from 'src/common/decorators/public.decorator';
-import { SeedInput } from './dtos/seed.input';
 import { runSeedDocs } from './docs/runSeed.docs';
+import { SeedArgs } from './dtos/args/seed.args';
 
 @Controller('seed')
 export class SeedResolver {
@@ -11,7 +11,7 @@ export class SeedResolver {
 
     @Public()
     @Mutation(() => Boolean, runSeedDocs)
-    async runSeed(@Args('seed_options') seedOptions: SeedInput): Promise<boolean> {
+    async runSeed(@Args() seedOptions: SeedArgs): Promise<boolean> {
         await this.seedService.runSeed(seedOptions);
         return true;
     }
