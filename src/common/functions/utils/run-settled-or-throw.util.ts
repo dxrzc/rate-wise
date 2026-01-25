ef9e01ca20e893636eab26a6fe52fbece0106d1a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-01-24 21:05:32 -0500

    feat(common): update **runSettledOrThrow** to throw an **AggregateError** upon multiple rejections

--- src/common/functions/utils/run-settled-or-throw.util.ts
+++ src/common/functions/utils/run-settled-or-throw.util.ts
@@ -1,7 +1,17 @@
 export async function runSettledOrThrow<T = unknown[]>(promises: Promise<unknown>[]) {
     const results = await Promise.allSettled(promises);
-    const rejection = results.find((r) => r.status === 'rejected');
-    if (rejection) throw rejection.reason;
-    // Extract and return only the fulfilled values
-    return <T>results.filter((r) => r.status === 'fulfilled').map((r) => r.value);
+    const rejections = results.filter((r): r is PromiseRejectedResult => r.status === 'rejected');
+    if (rejections.length === 1) throw rejections[0].reason;
+    if (rejections.length > 1) {
+        throw new AggregateError(
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+            rejections.map((r) => r.reason),
+            'One or more concurrent operations failed',
+        );
+    }
+    return <T>(
+        results
+            .filter((r): r is PromiseFulfilledResult<unknown> => r.status === 'fulfilled')
+            .map((r) => r.value)
+    );
 }
