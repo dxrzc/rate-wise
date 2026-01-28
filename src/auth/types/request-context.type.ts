1a52b120a6a8413ef207bff7d69c99b88f6c1d07
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-01-28 13:30:41 -0500

    docs(auth): add comments in **RequestContext** type

--- src/auth/types/request-context.type.ts
+++ src/auth/types/request-context.type.ts
@@ -2,6 +2,11 @@ import { Request } from 'express';
 import { ISessionData } from '../interfaces/session-data.interface';
 import { AuthenticatedUser } from 'src/common/interfaces/user/authenticated-user.interface';
 
-export type RequestContext = Request & { session: ISessionData } & {
-    readonly user: AuthenticatedUser;
+/**
+ * Defines the request object with authentiuser data and session
+ */
+export type RequestContext = Request & {
+    readonly session: ISessionData; // express-session
+} & {
+    readonly user: AuthenticatedUser; // AuthGuard
 };
