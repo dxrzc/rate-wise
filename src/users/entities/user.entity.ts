20499fd47b39931ab3f9102503b484d4e46bd6d8
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-03-27 22:08:18 -0500

    feat(users): update user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,16 +3,15 @@ import { Item } from 'src/items/entities/item.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { UserRole } from '../enums/user-role.enum';
 import { AccountStatus } from '../enums/account-status.enum';
-import { AUTH_RULES } from 'src/auth/policy/auth.rules';
 import { Review } from 'src/reviews/entities/review.entity';
 import { Vote } from 'src/votes/entities/vote.entity';
 
 @Entity('account')
 export class User extends BaseEntity {
-    @Column({ type: 'varchar', unique: true, length: AUTH_RULES.USERNAME.MAX })
+    @Column({ type: 'text' })
     username!: string;
 
-    @Column({ type: 'varchar', unique: true, length: AUTH_RULES.EMAIL.MAX })
+    @Column({ type: 'text' })
     email!: string;
 
     @Column({ type: 'text', name: 'password_hash' })
@@ -21,7 +20,7 @@ export class User extends BaseEntity {
     @Column({
         type: 'enum',
         enum: UserRole,
-        enumName: 'account_role_enum',
+        enumName: 'account_roles',
         default: [UserRole.REVIEWER, UserRole.CREATOR],
         array: true,
     })
@@ -30,7 +29,7 @@ export class User extends BaseEntity {
     @Column({
         type: 'enum',
         enum: AccountStatus,
-        enumName: 'account_status_enum',
+        enumName: 'account_statuses',
         default: AccountStatus.PENDING_VERIFICATION,
     })
     status!: AccountStatus;
