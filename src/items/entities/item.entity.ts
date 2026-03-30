d92254e88daf8ab2e477365edd68bbfb2ae48716
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-03-29 20:22:31 -0500

    feat(item): update item entity class

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,27 +1,20 @@
 import { User } from 'src/users/entities/user.entity';
 import { BaseEntity } from 'src/common/entites/base.entity';
 import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
-import { ITEM_RULES } from '../policy/items.rules';
 import { Review } from 'src/reviews/entities/review.entity';
 
 @Entity()
 export class Item extends BaseEntity {
-    @Column('varchar', {
-        unique: true,
-        length: ITEM_RULES.TITLE.MAX,
-    })
+    @Column('text')
     title!: string;
 
     @Column('text')
     description!: string;
 
-    @Column('varchar', { length: ITEM_RULES.CATEGORY.MAX })
+    @Column('text')
     category!: string;
 
-    @Column('varchar', {
-        array: true,
-        length: ITEM_RULES.TAGS.TAG_MAX_LENGTH,
-    })
+    @Column('text', { array: true })
     tags!: string[];
 
     @OneToMany(() => Review, (review) => review.item)
@@ -34,6 +27,6 @@ export class Item extends BaseEntity {
     @JoinColumn({ name: 'account_id' })
     user!: User;
 
-    @Column({ name: 'account_id' })
+    @Column({ name: 'created_by' })
     createdBy!: string;
 }
