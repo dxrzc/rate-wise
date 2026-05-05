2e74397f2178165b8cc4de952738dd652b25b695
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-05-04 20:01:31 -0500

    feat(migration): update data source to use TypeScript paths for entities and migrations

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,9 +1,13 @@
 import { DataSource } from 'typeorm';
+import { join } from 'path';
+
+const migrationsPath = join(__dirname, 'migrations', '*.ts');
+const entitiesPath = join(__dirname, '..', 'src', '**', '*.entity.ts');
 
 export default new DataSource({
     url: process.env.POSTGRES_URI,
     synchronize: false,
-    entities: ['dist/**/*.entity.js'],
+    entities: [entitiesPath],
     type: 'postgres',
-    migrations: ['dist/db/migrations/*.js'],
+    migrations: [migrationsPath],
 });
