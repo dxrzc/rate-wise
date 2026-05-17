ecaecf728f062d0cd8823568e1a4b0473c8d0611
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-05-17 13:14:14 -0500

    feat(frontend): configure apollo client

---
+++ frontend/src/app/ApolloWrapper.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { HttpLink } from '@apollo/client';
+import {
+    ApolloNextAppProvider,
+    ApolloClient,
+    InMemoryCache,
+} from '@apollo/client-integration-nextjs';
+
+// TODO: envs
+const API_URL = 'http://localhost:3000/graphql';
+
+function makeClient() {
+    const httpLink = new HttpLink({ uri: API_URL });
+    return new ApolloClient({
+        cache: new InMemoryCache(),
+        link: httpLink,
+    });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+    return <ApolloNextAppProvider makeClient={makeClient}>{children}</ApolloNextAppProvider>;
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,16 @@
 import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { ApolloWrapper } from './ApolloWrapper';
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className={cn('font-sans', geist.variable)}>
-            <body className="bg-background text-foreground antialiased">{children}</body>
+            <body className="bg-background text-foreground antialiased">
+                <ApolloWrapper>{children}</ApolloWrapper>
+            </body>
         </html>
     );
 }
