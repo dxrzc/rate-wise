749956fa28fa4f59a5c13e016c73293dd7175039
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-05-21 12:55:07 -0500

    feat(frontend): return server's error message directly

--- frontend/src/features/auth/hooks/useRegister.hook.ts
+++ frontend/src/features/auth/hooks/useRegister.hook.ts
@@ -2,13 +2,18 @@ import { useMutation } from '@apollo/client/react';
 import { SIGN_UP } from '../api/sign-up.mutation';
 import { SignUpMutation, SignUpMutationVariables } from '@/types/__generated__/graphql';
 import { SignUpData } from '../types/signUpData.type';
+import { getErrorMessage } from '@/utils/get-error-message.util';
 
 export function useRegister() {
     const [registerAction, { data, error }] = useMutation<SignUpMutation, SignUpMutationVariables>(
         SIGN_UP,
     );
 
-    const handleRegister = async ({ username, email, password }: SignUpData): Promise<boolean> => {
+    const handleRegister = async ({
+        username,
+        email,
+        password,
+    }: SignUpData): Promise<string | null> => {
         const response = await registerAction({
             variables: {
                 userData: {
@@ -18,8 +23,7 @@ export function useRegister() {
                 },
             },
         });
-        if (!response.error) return true;
-        return false;
+        return response.error ? getErrorMessage(error) : null;
     };
 
     return { handleRegister, data, error };
