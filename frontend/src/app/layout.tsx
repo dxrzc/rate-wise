f15cb93693294f2016da223677075395c4c8fe69
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-07-23 22:36:30 -0500

    feat(frontend): initialize shadcn

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,15 @@
 import { ApolloWrapper } from './ApolloWrapper';
+import { Inter, Public_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const publicSansHeading = Public_Sans({subsets:['latin'],variable:'--font-heading'});
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className={cn("font-sans", inter.variable, publicSansHeading.variable)}>
             <body>
                 <ApolloWrapper>{children}</ApolloWrapper>
             </body>
