cf83f20f4e4b7456ca4e4d070052f6f912803553
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-07-29 20:26:57 -0500

    feat(frontend): add toaster component in layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,19 @@
 import './globals.css';
 import { ApolloWrapper } from './ApolloWrapper';
-import { Inter, Public_Sans } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { Inter, Public_Sans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import { Toaster } from '@/components/ui/sonner';
 
-const publicSansHeading = Public_Sans({subsets:['latin'],variable:'--font-heading'});
+const publicSansHeading = Public_Sans({ subsets: ['latin'], variable: '--font-heading' });
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className={cn("font-sans", inter.variable, publicSansHeading.variable)}>
+        <html lang="en" className={cn('font-sans', inter.variable, publicSansHeading.variable)}>
             <body>
                 <ApolloWrapper>{children}</ApolloWrapper>
+                <Toaster></Toaster>
             </body>
         </html>
     );
